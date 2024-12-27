dacd4e5fa75081147d08e7316ce032778f3b2db7
jisu <dlawotn3@naver.com>
2024-12-27 10:18:57 +0900

    feat: add JwtTokenProvider

---
+++ src/main/kotlin/com/dobby/backend/infrastructure/token/JwtTokenProvider.kt
@@ -0,0 +1,96 @@
+package com.dobby.backend.infrastructure.token
+
+import com.dobby.backend.domain.exception.*
+import com.dobby.backend.infrastructure.configuration.TokenProperties
+import io.jsonwebtoken.ExpiredJwtException
+import io.jsonwebtoken.JwtException
+import io.jsonwebtoken.Jwts
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.stereotype.Component
+import java.util.*
+import javax.crypto.SecretKey
+import javax.crypto.spec.SecretKeySpec
+
+@Component
+class JWTTokenProvider(
+    private val tokenProperties: TokenProperties
+) {
+    private final val signKey: SecretKey = SecretKeySpec(tokenProperties.secretKey.toByteArray(), "AES")
+    private val jwtParser = Jwts
+        .parser()
+        .decryptWith(signKey)
+        .build()
+
+    fun generateAccessToken(authentication: Authentication): String {
+        val authorities = authentication.authorities.joinToString(",") {
+            it.authority
+        }
+        return Jwts.builder()
+            .header()
+            .add(TOKEN_TYPE_HEADER_KEY, ACCESS_TOKEN_TYPE_VALUE)
+            .and()
+            .claims()
+            .add(USER_ID_CLAIM_KEY, authentication.name)
+            .add(AUTHORITIES_CLAIM_KEY, authorities)
+            .and()
+            .expiration(generateAccessTokenExpiration())
+            .encryptWith(signKey, Jwts.ENC.A128CBC_HS256)
+            .compact()
+    }
+
+    fun generateRefreshToken(authentication: Authentication): String {
+        return Jwts.builder()
+            .header()
+            .add(TOKEN_TYPE_HEADER_KEY, REFRESH_TOKEN_TYPE_VALUE)
+            .and()
+            .claims()
+            .add(USER_ID_CLAIM_KEY, authentication.name)
+            .and()
+            .expiration(generateRefreshTokenExpiration())
+            .encryptWith(signKey, Jwts.ENC.A128CBC_HS256)
+            .compact()
+    }
+
+    fun parseAuthentication(accessToken: String): Authentication {
+        try {
+            val claims = jwtParser.parseEncryptedClaims(accessToken)
+            val tokenType = claims.header[TOKEN_TYPE_HEADER_KEY] ?: throw InvalidTokenTypeException()
+            if (tokenType != ACCESS_TOKEN_TYPE_VALUE) throw InvalidTokenTypeException()
+
+            val userId = claims.payload[USER_ID_CLAIM_KEY] as? String ?: throw RuntimeException()
+            val authorities = claims.payload[AUTHORITIES_CLAIM_KEY]?.toString()
+                ?.split(",")
+                ?.map { SimpleGrantedAuthority(it) }
+                ?: emptyList()
+
+            return UsernamePasswordAuthenticationToken(userId, accessToken, authorities)
+        } catch (e: ExpiredJwtException) {
+            throw AuthenticationTokenExpiredException()
+        } catch (e: JwtException) {
+            throw AuthenticationTokenNotValidException()
+        }
+    }
+
+    fun getMemberIdFromRefreshToken(refreshToken: String): String {
+        val claims = jwtParser.parseEncryptedClaims(refreshToken)
+        val tokenType = claims.header[TOKEN_TYPE_HEADER_KEY] ?: throw RuntimeException()
+        if (tokenType != REFRESH_TOKEN_TYPE_VALUE) throw RuntimeException()
+
+        return claims.payload[USER_ID_CLAIM_KEY] as? String ?: throw RuntimeException()
+    }
+
+    private fun generateAccessTokenExpiration() = Date(System.currentTimeMillis() + tokenProperties.expiration.access * 1000)
+
+    private fun generateRefreshTokenExpiration() = Date(System.currentTimeMillis() + tokenProperties.expiration.refresh * 1000)
+
+    companion object {
+        const val USER_ID_CLAIM_KEY = "user_id"
+        const val AUTHORITIES_CLAIM_KEY = "authorities"
+
+        const val TOKEN_TYPE_HEADER_KEY = "token_type"
+        const val ACCESS_TOKEN_TYPE_VALUE = "access_token"
+        const val REFRESH_TOKEN_TYPE_VALUE = "refresh_token"
+    }
+}
