92912e832309f97930b2ef40cf91bb6df4bdfbf9
jisu <dlawotn3@naver.com>
2024-12-31 13:45:41 +0900

    feat: add EnableConfigurationProperties for S3Properties

--- src/main/kotlin/com/dobby/backend/config/ApplicationConfig.kt
+++ src/main/kotlin/com/dobby/backend/config/ApplicationConfig.kt
@@ -1,10 +1,11 @@
 package com.dobby.backend.config
 
+import com.dobby.backend.infrastructure.config.S3Properties
 import com.dobby.backend.infrastructure.config.TokenProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@EnableConfigurationProperties(TokenProperties::class)
+@EnableConfigurationProperties(TokenProperties::class, S3Properties::class)
 class ApplicationConfig {
 }
