c88d3790aff1b8a3924a3a0367838d20ba1fb1ad
Jisu Lim <69844138+Ji-soo708@users.noreply.github.com>
2025-01-02 13:59:36 +0900

    [YS-37] chore: S3 연동 및 Pre-SignedUrl 로직 구현 (#14)
    * chore: add aws s3 dependency
    
    * chore: add S3 Config
    
    * feat: add EnableConfigurationProperties for S3Properties
    
    * refact: move properties file to properties dir
    
    * feat: add S3Gateway
    
    * feat: add S3PreSignedUrlProvider
    
    * refact: move response file to appropriate directories
    
    * feat: add InvalidInputException to generateUniqueImageName logic
    
    * test: add S3PreSignedUrlProvider test code
    
    * refact: separate S3Gateway implementation and provider for better responsibility segregation
    
    * style: delete unused import

---
+++ src/main/kotlin/com/dobby/backend/infrastructure/config/S3Config.kt
@@ -0,0 +1,35 @@
+package com.dobby.backend.infrastructure.config
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import com.dobby.backend.infrastructure.config.properties.S3Properties
+import lombok.RequiredArgsConstructor
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableConfigurationProperties(S3Properties::class)
+@RequiredArgsConstructor
+class S3Config {
+
+    private val properties: S3Properties
+
+    @Autowired
+    constructor(properties: S3Properties) {
+        this.properties = properties
+    }
+
+    @Bean
+    fun amazonS3Client(): AmazonS3? {
+        val awsCredentialsProvider = BasicAWSCredentials(properties.credentials.accessKey, properties.credentials.secretKey)
+
+        return AmazonS3ClientBuilder.standard()
+            .withRegion(properties.region.static)
+            .withCredentials(AWSStaticCredentialsProvider(awsCredentialsProvider))
+            .build()
+    }
+}
---
+++ src/main/kotlin/com/dobby/backend/infrastructure/s3/S3PreSignedUrlProvider.kt
@@ -0,0 +1,60 @@
+package com.dobby.backend.infrastructure.s3
+
+import com.amazonaws.AmazonServiceException
+import com.amazonaws.HttpMethod
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.Headers
+import com.amazonaws.services.s3.model.CannedAccessControlList
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest
+import com.dobby.backend.domain.exception.InvalidInputException
+import com.dobby.backend.presentation.api.dto.response.PreSignedUrlResponse
+import com.dobby.backend.util.generateULID
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import java.util.*
+
+@Component
+class S3PreSignedUrlProvider(
+    private val amazonS3Client: AmazonS3,
+) {
+
+    @Value("\${cloud.aws.s3.bucket}")
+    lateinit var bucket: String
+
+    fun getPreSignedUrl(fileName: String): PreSignedUrlResponse {
+        val generatePreSignedUrlRequest = getGenerateImagePreSignedUrlRequest(fileName)
+        return PreSignedUrlResponse(generatePreSignedUrl(generatePreSignedUrlRequest))
+    }
+
+    private fun generatePreSignedUrl(generatePresignedUrlRequest: GeneratePresignedUrlRequest): String {
+        return try {
+            amazonS3Client.generatePresignedUrl(generatePresignedUrlRequest).toString()
+        } catch (e: AmazonServiceException) {
+            throw IllegalStateException("Pre-signed Url 생성 실패했습니다.")
+        }
+    }
+
+    private fun getGenerateImagePreSignedUrlRequest(fileName: String): GeneratePresignedUrlRequest {
+        val savedImageName = generateUniqueImageName(fileName)
+        val savedImagePath = "images/$savedImageName"
+        return getPreSignedUrlRequest(bucket, savedImagePath)
+    }
+
+    private fun getPreSignedUrlRequest(bucket: String, fileName: String): GeneratePresignedUrlRequest {
+        return GeneratePresignedUrlRequest(bucket, fileName)
+            .withMethod(HttpMethod.PUT)
+            .withExpiration(Date(System.currentTimeMillis() + 180000))
+            .apply {
+                addRequestParameter(Headers.S3_CANNED_ACL, CannedAccessControlList.PublicRead.toString())
+            }
+    }
+
+    private fun generateUniqueImageName(fileName: String): String {
+        val lastDotIndex = fileName.lastIndexOf(".")
+        if (lastDotIndex == -1 || lastDotIndex == fileName.length - 1) {
+            throw InvalidInputException()
+        }
+        val ext = fileName.substring(lastDotIndex)
+        return "${generateULID()}$ext"
+    }
+}
