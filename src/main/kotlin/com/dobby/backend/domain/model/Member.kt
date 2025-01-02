65ffa0cb32859dfb9ad5a7a644953a054e2af414
jisu <dlawotn3@naver.com>
2025-01-02 11:59:57 +0900

    refact: refactor member domain

--- src/main/kotlin/com/dobby/backend/domain/model/Member.kt
+++ src/main/kotlin/com/dobby/backend/domain/model/Member.kt
@@ -1,20 +1,40 @@
 package com.dobby.backend.domain.model
 
+import com.dobby.backend.infrastructure.database.entity.enum.MemberStatus
+import com.dobby.backend.infrastructure.database.entity.enum.ProviderType
+import com.dobby.backend.infrastructure.database.entity.enum.RoleType
+import java.time.LocalDate
+
 data class Member(
     val memberId: Long,
     val name: String,
-    val email: String,
+    val oauthEmail: String,
+    val contactEmail: String,
+    val provider: ProviderType,
+    val status: MemberStatus,
+    val role: RoleType,
+    val birthDate: LocalDate,
 ) {
 
     companion object {
         fun newMember(
             memberId: Long,
             name: String,
-            email: String,
+            oauthEmail: String,
+            contactEmail: String,
+            provider: ProviderType,
+            status: MemberStatus,
+            role: RoleType,
+            birthDate: LocalDate,
         ) = Member(
             memberId = memberId,
             name = name,
-            email = email,
+            oauthEmail = oauthEmail,
+            contactEmail = contactEmail,
+            provider = provider,
+            status = status,
+            role = role,
+            birthDate = birthDate,
         )
     }
 }
