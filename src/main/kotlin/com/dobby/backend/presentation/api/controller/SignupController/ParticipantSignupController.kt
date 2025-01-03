dcf3ac680d6942658e5af095d43ef8e0d3f2dd39
베뉴 <christer10@ewhain.net>
2025-01-04 01:06:54 +0900

    style: rename API endpoint signup to meet the convention rule
    - 회원가입 API endpoint 관련하여 기존 `/join` → `/signup` 으로 개선
    - 코드 컨벤션을 위한 API endpoint 개선

--- src/main/kotlin/com/dobby/backend/presentation/api/controller/SignupController/ParticipantSignupController.kt
+++ src/main/kotlin/com/dobby/backend/presentation/api/controller/SignupController/ParticipantSignupController.kt
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*
 class ParticipantSignupController(
     private val signupService: SignupService
 ) {
-    @PostMapping("/join")
+    @PostMapping("/signup")
     @Operation(
         summary = "참여자 회원가입 API- OAuth 로그인 필수",
         description = "참여자 OAuth 로그인 실패 시, 리다이렉팅하여 참여자 회원가입하는 API입니다."
