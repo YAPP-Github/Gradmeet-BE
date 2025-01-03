c5b55014d9a9ad0dbe16a858e5721c3bf9416fce
jisu <dlawotn3@naver.com>
2025-01-04 01:57:57 +0900

    test: add role validation test code

--- src/test/kotlin/com/dobby/backend/domain/usecase/GenerateTestTokenTest.kt
+++ src/test/kotlin/com/dobby/backend/domain/usecase/GenerateTestTokenTest.kt
@@ -1,26 +1,36 @@
 package com.dobby.backend.domain.usecase
 
 import com.dobby.backend.application.usecase.GenerateTestToken
+import com.dobby.backend.domain.gateway.MemberGateway
 import io.kotest.core.spec.style.BehaviorSpec
 import com.dobby.backend.domain.gateway.TokenGateway
+import com.dobby.backend.domain.model.Member
+import com.dobby.backend.infrastructure.database.entity.enum.MemberStatus
+import com.dobby.backend.infrastructure.database.entity.enum.ProviderType
+import com.dobby.backend.infrastructure.database.entity.enum.RoleType
 import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
+import java.time.LocalDate
 
 class GenerateTestTokenTest: BehaviorSpec({
     val tokenGateway = mockk<TokenGateway>()
-    val generateTestToken = GenerateTestToken(tokenGateway)
+    val memberGateway = mockk<MemberGateway>()
+    val generateTestToken = GenerateTestToken(tokenGateway, memberGateway)
 
     given("memberId가 주어졌을 때") {
-        val memberId = 123L
+        val member = Member(memberId = 1, oauthEmail = "dlawotn3@naver.com", contactEmail = "dlawotn3@naver.com",
+            provider = ProviderType.NAVER, role = RoleType.PARTICIPANT, name = "dobby",
+            birthDate = LocalDate.of(2000, 7, 8), status = MemberStatus.ACTIVE)
         val accessToken = "testAccessToken"
         val refreshToken = "testRefreshToken"
 
-        every { tokenGateway.generateAccessToken(memberId) } returns accessToken
-        every { tokenGateway.generateRefreshToken(memberId) } returns refreshToken
+        every { tokenGateway.generateAccessToken(member) } returns accessToken
+        every { tokenGateway.generateRefreshToken(member) } returns refreshToken
+        every { memberGateway.getById(1) } returns member
 
         `when`("execute가 호출되면") {
-            val input = GenerateTestToken.Input(memberId)
+            val input = GenerateTestToken.Input(member.memberId)
             val result = generateTestToken.execute(input)
 
             then("생성된 accessToken과 refreshToken이 반환되어야 한다") {
--- src/test/kotlin/com/dobby/backend/domain/usecase/GenerateTokenWithRefreshTokenTest.kt
+++ src/test/kotlin/com/dobby/backend/domain/usecase/GenerateTokenWithRefreshTokenTest.kt
@@ -1,25 +1,36 @@
 package com.dobby.backend.domain.usecase
 
+import com.dobby.backend.application.usecase.GenerateTestToken
 import com.dobby.backend.application.usecase.GenerateTokenWithRefreshToken
+import com.dobby.backend.domain.gateway.MemberGateway
 import com.dobby.backend.domain.gateway.TokenGateway
+import com.dobby.backend.domain.model.Member
+import com.dobby.backend.infrastructure.database.entity.enum.MemberStatus
+import com.dobby.backend.infrastructure.database.entity.enum.ProviderType
+import com.dobby.backend.infrastructure.database.entity.enum.RoleType
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
+import java.time.LocalDate
 
 class GenerateTokenWithRefreshTokenTest : BehaviorSpec({
     val tokenGateway = mockk<TokenGateway>()
-    val generateTokenWithRefreshToken = GenerateTokenWithRefreshToken(tokenGateway)
+    val memberGateway = mockk<MemberGateway>()
+    val generateTokenWithRefreshToken = GenerateTokenWithRefreshToken(tokenGateway, memberGateway)
 
     given("유효한 리프레시 토큰이 주어졌을 때") {
         val validRefreshToken = "validRefreshToken"
-        val memberId = 123L
+        val member = Member(memberId = 1, oauthEmail = "dlawotn3@naver.com", contactEmail = "dlawotn3@naver.com",
+            provider = ProviderType.NAVER, role = RoleType.PARTICIPANT, name = "dobby",
+            birthDate = LocalDate.of(2000, 7, 8), status = MemberStatus.ACTIVE)
         val accessToken = "newAccessToken"
         val newRefreshToken = "newRefreshToken"
 
-        every { tokenGateway.extractMemberIdFromRefreshToken(validRefreshToken) } returns memberId.toString()
-        every { tokenGateway.generateAccessToken(memberId) } returns accessToken
-        every { tokenGateway.generateRefreshToken(memberId) } returns newRefreshToken
+        every { tokenGateway.extractMemberIdFromRefreshToken(validRefreshToken) } returns member.memberId.toString()
+        every { tokenGateway.generateAccessToken(member) } returns accessToken
+        every { tokenGateway.generateRefreshToken(member) } returns newRefreshToken
+        every { memberGateway.getById(1) } returns member
 
         `when`("execute가 호출되면") {
             val input = GenerateTokenWithRefreshToken.Input(refreshToken = validRefreshToken)
@@ -28,7 +39,7 @@ class GenerateTokenWithRefreshTokenTest : BehaviorSpec({
             then("accessToken과 refreshToken이 생성되고, memberId가 포함된다") {
                 result.accessToken shouldBe accessToken
                 result.refreshToken shouldBe newRefreshToken
-                result.memberId shouldBe memberId
+                result.memberId shouldBe member.memberId
             }
         }
     }
