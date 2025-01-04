01ae7188071cc4d1eff003e2bc68a60aa0796d07
Jisu Lim <69844138+Ji-soo708@users.noreply.github.com>
2025-01-04 13:51:42 +0900

    [YS-70] feat: JWT 토큰에 사용자 권한(RoleType) 정보 포함 (#18)
    * refact: delete unused file
    
    * refact: add roleName to Enum
    
    * feat: add memberRole to jwt token
    
    * test: add role validation test code
    
    * test: move usecase's test code to application package
    
    * style: delete unused import
    
    * refact: refactor exception handling to be more granular for token validation
    
    * style: rename JpaMemberRepository to MemberRepository for better clarity

--- src/main/kotlin/com/dobby/backend/domain/gateway/TokenGateway.kt
+++ src/main/kotlin/com/dobby/backend/domain/gateway/TokenGateway.kt
@@ -1,7 +1,9 @@
 package com.dobby.backend.domain.gateway
 
+import com.dobby.backend.domain.model.Member
+
 interface TokenGateway {
-    fun generateAccessToken(memberId: Long): String
-    fun generateRefreshToken(memberId: Long): String
+    fun generateAccessToken(member: Member): String
+    fun generateRefreshToken(member: Member): String
     fun extractMemberIdFromRefreshToken(token: String): String
 }
--- src/main/kotlin/com/dobby/backend/infrastructure/gateway/MemberGatewayImpl.kt
+++ src/main/kotlin/com/dobby/backend/infrastructure/gateway/MemberGatewayImpl.kt
@@ -3,15 +3,15 @@ package com.dobby.backend.infrastructure.gateway
 import com.dobby.backend.domain.gateway.MemberGateway
 import com.dobby.backend.domain.model.Member
 import com.dobby.backend.infrastructure.database.entity.MemberEntity
-import com.dobby.backend.infrastructure.database.repository.MemberJpaRepository
+import com.dobby.backend.infrastructure.database.repository.MemberRepository
 import org.springframework.stereotype.Component
 
 @Component
 class MemberGatewayImpl(
-    private val jpaMemberRepository: MemberJpaRepository,
+    private val memberRepository: MemberRepository,
 ) : MemberGateway {
     override fun getById(memberId: Long): Member {
-        return jpaMemberRepository
+        return memberRepository
             .getReferenceById(memberId)
             .let(MemberEntity::toDomain)
     }
