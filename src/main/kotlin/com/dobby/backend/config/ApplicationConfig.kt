8a2766603286e8033ceccd3b9438a965d14bd987
jisu <dlawotn3@naver.com>
2025-01-09 18:31:27 +0900

    feat: add EnableConfigurationProperties for OAuth

--- src/main/kotlin/com/dobby/backend/config/ApplicationConfig.kt
+++ src/main/kotlin/com/dobby/backend/config/ApplicationConfig.kt
@@ -5,11 +5,17 @@ import com.dobby.backend.application.usecase.FetchGoogleUserInfoUseCase
 import com.dobby.backend.application.usecase.FetchNaverUserInfoUseCase
 import com.dobby.backend.application.usecase.GenerateTestTokenUseCase
 import com.dobby.backend.application.usecase.GenerateTokenWithRefreshTokenUseCase
+import com.dobby.backend.infrastructure.config.properties.GoogleAuthProperties
+import com.dobby.backend.infrastructure.config.properties.NaverAuthProperties
+import com.dobby.backend.infrastructure.config.properties.S3Properties
+import com.dobby.backend.infrastructure.config.properties.TokenProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class AppConfig {
+@EnableConfigurationProperties(TokenProperties::class, S3Properties::class, GoogleAuthProperties::class, NaverAuthProperties::class)
+class ApplicationConfig {
     @Bean
     fun authService(
         fetchGoogleUserInfoUseCase: FetchGoogleUserInfoUseCase,
