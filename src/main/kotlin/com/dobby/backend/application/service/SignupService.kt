737f93369c4899b8c1ac8d9ba6795dec90abef9b
베뉴 <christer10@ewhain.net>
2025-01-09 10:17:01 +0900

    fix: resolve merge conflicts

--- src/main/kotlin/com/dobby/backend/application/service/SignupService.kt
+++ src/main/kotlin/com/dobby/backend/application/service/SignupService.kt
@@ -1,13 +1,8 @@
 package com.dobby.backend.application.service
 
-<<<<<<< HEAD
 import com.dobby.backend.application.usecase.signupUseCase.CreateResearcherUseCase
 import com.dobby.backend.application.usecase.signupUseCase.ParticipantSignupUseCase
 import com.dobby.backend.application.usecase.signupUseCase.VerifyResearcherEmailUseCase
-=======
-import com.dobby.backend.application.usecase.signupUseCase.ParticipantSignupUseCase
-
->>>>>>> 03b03d56d8a6f26e26d51fe41356c6b74bbd3342
 import com.dobby.backend.domain.exception.EmailNotValidateException
 import com.dobby.backend.presentation.api.dto.request.signup.ParticipantSignupRequest
 import com.dobby.backend.presentation.api.dto.request.signup.ResearcherSignupRequest
