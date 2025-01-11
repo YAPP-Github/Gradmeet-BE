b0ad193a1bd626d77acfe244d2c2403ec859b5b3
jisu <dlawotn3@naver.com>
2025-01-11 21:45:35 +0900

    refact: remove AuthenticationUtils and use getCurrentMemberId directly

--- src/main/kotlin/com/dobby/backend/util/AuthenticationUtils.kt
+++ src/main/kotlin/com/dobby/backend/util/AuthenticationUtils.kt
@@ -1,20 +1,5 @@
 package com.dobby.backend.util
 
-import com.dobby.backend.infrastructure.database.entity.member.MemberEntity
-import org.springframework.security.core.Authentication
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
 
-object AuthenticationUtils {
-    fun createAuthentication(member: MemberEntity): Authentication {
-        return UsernamePasswordAuthenticationToken(
-            member,
-            null,
-            emptyList()
-        ) as Authentication
-    }
-    fun getCurrentMemberId(): Long {
-        val authentication = SecurityContextHolder.getContext().authentication
-        return authentication.name.toLong()
-    }
-}
+fun getCurrentMemberId() = SecurityContextHolder.getContext().authentication.name.toLong()
