7c3f7a92865b6d1ffcee4f951992a6c43dde4f37
jisu <dlawotn3@naver.com>
2025-01-14 21:58:55 +0900

    refact: refactor request, response dto

--- src/main/kotlin/com/dobby/backend/presentation/api/dto/request/signup/ParticipantSignupRequest.kt
+++ src/main/kotlin/com/dobby/backend/presentation/api/dto/request/signup/ParticipantSignupRequest.kt
@@ -3,8 +3,8 @@ package com.dobby.backend.presentation.api.dto.request.signup
 import com.dobby.backend.infrastructure.database.entity.enum.GenderType
 import com.dobby.backend.infrastructure.database.entity.enum.MatchType
 import com.dobby.backend.infrastructure.database.entity.enum.ProviderType
-import com.dobby.backend.infrastructure.database.entity.enum.areaInfo.Area
-import com.dobby.backend.infrastructure.database.entity.enum.areaInfo.Region
+import com.dobby.backend.presentation.api.dto.response.member.AddressInfoResponse
+import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
@@ -15,39 +15,38 @@ import java.time.LocalDate
 data class ParticipantSignupRequest(
     @Email(message = "OAuth 이메일이 유효하지 않습니다.")
     @NotBlank(message = "OAuth 이메일은 공백일 수 없습니다.")
+    @Schema(description = "OAuth 이메일")
     val oauthEmail: String,
 
     @NotBlank(message = "OAuth provider은 공백일 수 없습니다.")
+    @Schema(description = "OAuth provider")
     val provider: ProviderType,
 
     @Email(message= "연락 받을 이메일이 유효하지 않습니다.")
     @NotBlank(message = "연락 받을 이메일은 공백일 수 없습니다.")
+    @Schema(description = "연락 받을 이메일")
     val contactEmail: String,
 
     @NotBlank(message = "이름은 공백일 수 없습니다.")
+    @Schema(description = "이름")
     val name : String,
 
     @NotBlank(message = "성별은 공백일 수 없습니다.")
+    @Schema(description = "성별")
     val gender: GenderType,
 
     @Past @NotNull(message = "생년월일은 공백일 수 없습니다.")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Schema(description = "생년월일")
     val birthDate: LocalDate,
 
     @NotBlank(message = "거주 지역은 공백일 수 없습니다.")
-    var basicAddressInfo: AddressInfo,
+    @Schema(description = "기본 주소 정보")
+    var basicAddressInfo: AddressInfoResponse,
 
-    // 추가 활동 정보
-    var additionalAddressInfo: AddressInfo?,
+    @Schema(description = "추가 주소 정보")
+    var additionalAddressInfo: AddressInfoResponse?,
 
-    // 선호 실험 진행 방식
+    @Schema(description = "선호 실험 진행 방식")
     var matchType: MatchType,
-)
-
-data class AddressInfo(
-    @NotBlank(message = "시/도 정보는 공백일 수 없습니다.")
-    val region: Region,
-
-    @NotBlank(message = "시/군/구 정보는 공백일 수 없습니다.")
-    val area: Area
 )
