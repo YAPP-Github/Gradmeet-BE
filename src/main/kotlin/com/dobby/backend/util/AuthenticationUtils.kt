012afaa8bc7395cce3c97d57f2b53b2b51148684
jisu <dlawotn3@naver.com>
2025-01-19 15:03:10 +0900

    feat: add getCurrentMemberIdOrNull()

--- src/main/kotlin/com/dobby/backend/util/AuthenticationUtils.kt
+++ src/main/kotlin/com/dobby/backend/util/AuthenticationUtils.kt
@@ -3,3 +3,14 @@ package com.dobby.backend.util
 import org.springframework.security.core.context.SecurityContextHolder
 
 fun getCurrentMemberId() = SecurityContextHolder.getContext().authentication.name.toLong()
+
+fun getCurrentMemberIdOrNull(): Long? {
+    val authentication = SecurityContextHolder.getContext().authentication
+    val name = authentication?.name
+
+    return if (name != null && name != "anonymousUser") {
+        name.toLong()
+    } else {
+        null
+    }
+}
