b8aee193443a8818a643f146cfe9cdeb079fc603
jisu <dlawotn3@naver.com>
2025-01-20 14:25:10 +0900

    fix: update Naver API response and add response field

--- src/main/kotlin/com/dobby/backend/presentation/api/dto/response/auth/naver/NaverInfoResponse.kt
+++ src/main/kotlin/com/dobby/backend/presentation/api/dto/response/auth/naver/NaverInfoResponse.kt
@@ -3,6 +3,17 @@ package com.dobby.backend.presentation.api.dto.response.auth.naver
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class NaverInfoResponse(
+    @JsonProperty("resultcode")
+    val resultCode: String,
+
+    @JsonProperty("message")
+    val message: String,
+
+    @JsonProperty("response")
+    val response: NaverUserResponse
+)
+
+data class NaverUserResponse(
     @JsonProperty("email")
     val email: String
 )
