5df5bbc4a3311555d2e0979f6ea61747e0ad1454
jisu <dlawotn3@naver.com>
2025-01-27 17:31:24 +0900

    chore: delete dev profile in dockerfile

--- src/main/kotlin/com/dobby/backend/DobbyBackendApplication.kt
+++ src/main/kotlin/com/dobby/backend/DobbyBackendApplication.kt
@@ -7,7 +7,6 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.FilterType
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @ComponentScan(
 	includeFilters = [ComponentScan.Filter(
