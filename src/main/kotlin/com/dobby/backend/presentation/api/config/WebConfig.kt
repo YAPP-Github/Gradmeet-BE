9f2f990f979a0a0ee58ac5a6444df9ab857bca0a
Jisu Lim <69844138+Ji-soo708@users.noreply.github.com>
2025-01-28 15:01:24 +0900

    [YS-230] hotfix: CORS 설정 변경 (#64)
    * chore: add allowedOrigins env
    
    * refact: use allowedOrigins for stricter CORS domain management

--- src/main/kotlin/com/dobby/backend/presentation/api/config/WebConfig.kt
+++ src/main/kotlin/com/dobby/backend/presentation/api/config/WebConfig.kt
@@ -1,17 +1,23 @@
 package com.dobby.backend.presentation.api.config
 
+import com.dobby.backend.presentation.api.config.properties.AllowedOriginsProperties
+import lombok.RequiredArgsConstructor
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class WebConfig : WebMvcConfigurer {
+@RequiredArgsConstructor
+class WebConfig(
+    private val allowedOriginsProperties: AllowedOriginsProperties
+) : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
+        val allowedOrigins = allowedOriginsProperties.allowedOrigins
+
         registry.addMapping("/**")
             .allowedMethods("*")
-            .allowedOrigins(
-                "http://localhost:3000",
-                "http://localhost:3300",
-            )
+            .allowedOrigins(*allowedOrigins.toTypedArray())
+            .allowCredentials(true);
     }
 }
