52d4fb455b6d4a184ec851cdc49aa66ce7075f1b
jisu <dlawotn3@naver.com>
2025-01-28 15:22:49 +0900

    hotfix: add swagger cors configuration source

--- src/main/kotlin/com/dobby/backend/presentation/api/config/WebSecurityConfig.kt
+++ src/main/kotlin/com/dobby/backend/presentation/api/config/WebSecurityConfig.kt
@@ -5,6 +5,7 @@ import com.dobby.backend.domain.exception.UnauthorizedException
 import com.dobby.backend.infrastructure.token.JwtTokenProvider
 import com.dobby.backend.presentation.api.config.filter.JwtAuthenticationFilter
 import com.dobby.backend.presentation.api.config.filter.JwtOptionalAuthenticationFilter
+import com.dobby.backend.presentation.api.config.properties.AllowedOriginsProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.annotation.Order
@@ -14,11 +15,16 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import org.springframework.web.servlet.HandlerExceptionResolver
 
 @Configuration
 @EnableMethodSecurity
-class WebSecurityConfig {
+class WebSecurityConfig(
+    private val allowedOriginsProperties: AllowedOriginsProperties
+) {
     /**
      * Configures the security for authentication-related APIs, including login and signup.
      * These endpoints are publicly accessible without authentication.
@@ -115,10 +121,22 @@ class WebSecurityConfig {
     @Order(4)
     fun swaggerSecurityFilterChain(httpSecurity: HttpSecurity): SecurityFilterChain = httpSecurity
         .securityMatcher("/swagger-ui/**", "/v3/api-docs/**")
+        .cors { it.configurationSource(corsConfigurationSource()) }
         .csrf { it.disable() }
-        .cors(Customizer.withDefaults())
         .authorizeHttpRequests {
             it.requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
         }
         .build()
+
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = allowedOriginsProperties.allowedOrigins
+        configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        configuration.allowCredentials = true
+        configuration.allowedHeaders = listOf("*")
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
+    }
 }
