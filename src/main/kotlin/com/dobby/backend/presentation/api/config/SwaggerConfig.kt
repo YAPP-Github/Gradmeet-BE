66712ff288680cb41479e47628d6e3f973a893b7
jisu <dlawotn3@naver.com>
2025-01-28 15:38:27 +0900

    hotfix: add swagger server-url for cors

--- src/main/kotlin/com/dobby/backend/presentation/api/config/SwaggerConfig.kt
+++ src/main/kotlin/com/dobby/backend/presentation/api/config/SwaggerConfig.kt
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Configuration
 
 @OpenAPIDefinition(
     servers = [Server(
-        url = "/",
+        url = "\${swagger.server-url}",
         description = "Default Server URL"
     )],
     info = Info(
