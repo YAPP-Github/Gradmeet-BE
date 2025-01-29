984245bf4fb49d0e6df7b6484db968093cb9c687
Sujung Shin <113707388+chock-cho@users.noreply.github.com>
2025-01-29 11:12:21 +0900

    [YS-191] refact: 회원가입 로직 리팩터링 (#65)
    * refact: refact ParticipantSignup logic to meet the clean architecture
    principal
    
    - 참여자 회원가입 로직 리팩터링
    - `infrastructure` 계층의 `converter` 삭제
    - `mapper` 로 연결된 메서드 제거
    
    * refact: refact researcherSignup logic to meet the clean architecture
        principal
    
    - 연구자 회원가입 시 클린 아키텍처에 맞게 리팩터링
    - `infrastructure` 계층의 converter 사용 제거
    - entity레벨에서 `fromDomain()` `toDomain()` 메서드 추가
    
    * refact: delete unused mapper packages import
    
    * refact: delete unused mapper packages import
    
    * refact: encapsulate Signup logic in domain model
    
    - `memberId`, `status`, `createdAt` 기본값을 도메인에서 설정하게 변경
    - 유즈케이스에서는 변동 가능한 값만 주입하도록 개선
    - 객체 생성 로직을 캡슐화하여 비즈니스 로직에 집중할 수 있도록 함
    
    * refact: delete unused SingupMapper class
    
    * refact: set updatedAt on object creation on domain model
    
    * fix: delete outdated parameters

--- src/main/kotlin/com/dobby/backend/infrastructure/database/entity/member/ResearcherEntity.kt
+++ src/main/kotlin/com/dobby/backend/infrastructure/database/entity/member/ResearcherEntity.kt
@@ -1,5 +1,6 @@
 package com.dobby.backend.infrastructure.database.entity.member
 
+import com.dobby.backend.domain.model.member.Researcher
 import jakarta.persistence.*
 
 @Entity
@@ -28,4 +29,29 @@ class ResearcherEntity (
 
     @Column(name = "lab_info", length = 100, nullable = true)
     val labInfo : String?,
-)
+) {
+    companion object {
+        fun fromDomain(domain: Researcher): ResearcherEntity {
+            return ResearcherEntity(
+                id = domain.id,
+                member = MemberEntity.fromDomain(domain.member),
+                univEmail = domain.univEmail,
+                emailVerified = domain.emailVerified,
+                univName = domain.univName,
+                major = domain.major,
+                labInfo = domain.labInfo
+            )
+        }
+    }
+    fun toDomain(): Researcher {
+        return Researcher(
+            id = this.id,
+            member = this.member.toDomain(),
+            univEmail = this.univEmail,
+            emailVerified = this.emailVerified,
+            univName = this.univName,
+            major = this.major,
+            labInfo = this.labInfo,
+        )
+    }
+}
