10b73780b8ca5aa572d88423cfc4e454e2088d91
베뉴 <christer10@ewhain.net>
2025-01-29 10:46:05 +0900

    refact: delete unused SingupMapper class

--- src/main/kotlin/com/dobby/backend/application/mapper/SignupMapper.kt
+++
@@ -1,8 +0,0 @@
-package com.dobby.backend.application.mapper
-import com.dobby.backend.application.usecase.member.CreateResearcherUseCase
-import com.dobby.backend.application.usecase.member.CreateParticipantUseCase
-import com.dobby.backend.domain.model.member.Participant
-import com.dobby.backend.domain.model.member.Researcher
-
-object SignupMapper {
-}
