c9ae43488dd038577ddbacc634fc052d9d7c7147
Sujung Shin <113707388+chock-cho@users.noreply.github.com>
2025-01-30 22:15:02 +0900

    [YS-235] refact: 공고 등록/공고 수정 로직 리팩터링 (#71)
    * refact: delegate create object logic to domain model
    
    - 도메인 객체(`ExperimentPost` `TargetGroup` `ApplyMethod`) 생성 책임을
      도메인 계층에 위임
    
    * feat: delete converter and use `fromDomain()` `toDomain` method
    
    - 공고 생성 시, `fromDomain()` `toDomain` 메서드 이용하여 GatewayImpl
      구현
    - 공고 이미지 생성 시, 도메인 모델에 생성 작업 위임
    
    * refact: apply clean architecture principles to ExperimentPost usecases
    
    - `UpdateExperimentPostUseCase` `CreateExperimentUseCase` 리팩터링
    - 도메인 로직을 유즈케이스 계층으로 분리하여 SRP 준수
    - 데이터 변경 시 불변성을 유지하도록 `copy()` 이용하여 객체 갱신 개선
    - 외부 의존성 낮춤
    
    * fix: delete unused import files
    
    * test: add missing unit tests for CreateExperimentPostUseCase
    
    * fix: delete unused ExperimentMapper method
    
    * fix: delegate common validate logic to domain model layer

--- src/main/kotlin/com/dobby/backend/application/usecase/experiment/CreateExperimentPostUseCase.kt
+++ src/main/kotlin/com/dobby/backend/application/usecase/experiment/CreateExperimentPostUseCase.kt
@@ -16,7 +16,6 @@ import com.dobby.backend.infrastructure.database.entity.enums.TimeSlot
 import com.dobby.backend.infrastructure.database.entity.enums.areaInfo.Area
 import com.dobby.backend.infrastructure.database.entity.enums.areaInfo.Region
 import java.time.LocalDate
-import java.time.LocalDateTime
 
 class CreateExperimentPostUseCase(
     private val experimentPostGateway: ExperimentPostGateway,
@@ -84,20 +83,44 @@ class CreateExperimentPostUseCase(
 
     override fun execute(input: Input): Output {
         val member = memberGateway.getById(input.memberId)
-        validate(input, member)
+        validateMemberRole(member)
 
-        val targetGroup = createTargetGroup(input.targetGroupInfo)
-        val applyMethod = createApplyMethod(input.applyMethodInfo)
-        var experimentPost = createExperimentPost(
-            member,
-            input,
-            targetGroup,
-            applyMethod
+        val targetGroup = TargetGroup.newTargetGroup(
+            startAge = input.targetGroupInfo.startAge,
+            endAge = input.targetGroupInfo.endAge,
+            genderType = input.targetGroupInfo.genderType,
+            otherCondition = input.targetGroupInfo.otherCondition
         )
 
+        val applyMethod = ApplyMethod.newApplyMethod(
+            phoneNum = input.applyMethodInfo.phoneNum,
+            formUrl = input.applyMethodInfo.formUrl,
+            content = input.applyMethodInfo.content
+        )
+
+        val experimentPost = ExperimentPost.newExperimentPost(
+            member = member,
+            targetGroup = targetGroup,
+            applyMethod = applyMethod,
+            leadResearcher = input.leadResearcher,
+            title = input.title,
+            content = input.content,
+            reward = input.reward,
+            startDate = input.startDate,
+            endDate = input.endDate,
+            timeRequired = input.timeRequired,
+            count = input.count,
+            matchType = input.matchType,
+            univName = input.univName,
+            region = input.region,
+            area = input.area,
+            detailedAddress = input.detailedAddress,
+            alarmAgree = input.alarmAgree,
+            recruitStatus = true,
+            )
+
         val experimentImages = input.imageListInfo.images.map { imageUrl ->
-            ExperimentImage(
-                id = 0L,
+            ExperimentImage.newExperimentImage(
                 experimentPost = null,
                 imageUrl = imageUrl
             )
@@ -120,80 +143,8 @@ class CreateExperimentPostUseCase(
             )
         )
     }
-
-    private fun createTargetGroup(targetGroupInfo: TargetGroupInfo): TargetGroup {
-        return TargetGroup(
-            id = 0L,
-            startAge = targetGroupInfo.startAge,
-            endAge = targetGroupInfo.endAge,
-            genderType = targetGroupInfo.genderType,
-            otherCondition = targetGroupInfo.otherCondition
-        )
-    }
-
-    private fun createApplyMethod(applyMethodInfo: ApplyMethodInfo): ApplyMethod {
-        return ApplyMethod(
-            id = 0L,
-            phoneNum = applyMethodInfo.phoneNum,
-            formUrl = applyMethodInfo.formUrl,
-            content = applyMethodInfo.content
-        )
-    }
-
-    private fun createExperimentPost(
-        member: Member,
-        input: Input,
-        targetGroup: TargetGroup,
-        applyMethod: ApplyMethod,
-    ): ExperimentPost {
-        return  ExperimentPost(
-            member = member,
-            leadResearcher = input.leadResearcher,
-            id = 0L,
-            targetGroup = targetGroup,
-            applyMethod = applyMethod,
-            views = 0,
-            title = input.title,
-            content = input.content,
-            reward = input.reward,
-            startDate = input.startDate,
-            endDate = input.endDate,
-            timeRequired = input.timeRequired,
-            count = input.count,
-            matchType = input.matchType,
-            univName = input.univName,
-            region = input.region,
-            area = input.area,
-            detailedAddress = input.detailedAddress,
-            alarmAgree = input.alarmAgree,
-            recruitStatus = true,
-            images = mutableListOf(),
-            createdAt = LocalDateTime.now(),
-            updatedAt = LocalDateTime.now(),
-        )
-    }
-
-    private fun validate(input: Input, member: Member){
-        validateMemberRole(member)
-        validateImageListSize(input.imageListInfo)
-        validateOnlineMatchType(input)
-    }
-
     private fun validateMemberRole(member :Member){
         if(member.role != RoleType.RESEARCHER)
             throw PermissionDeniedException
-    }
-
-    private fun validateImageListSize(imageListInfo: ImageListInfo){
-        if(imageListInfo.images.size > 3)
-            throw ExperimentPostImageSizeException
-    }
-
-    private fun validateOnlineMatchType(input: Input){
-        if(input.matchType == MatchType.ONLINE){
-            if(input.univName != null || input.region != null || input.area != null) {
-                throw ExperimentPostInvalidOnlineRequestException
-            }
-        }
     }
 }
