c8360ab20f9da1a71ece9772a0b8d09a29cd9066
jisu <dlawotn3@naver.com>
2025-01-31 02:18:30 +0900

    fix: specify docker-compose-dev.yml path in deploy script

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -6,13 +6,13 @@ IS_GREEN_EXIST=$(docker ps | grep green)
 if [[ -z "$IS_GREEN_EXIST" ]]; then
   echo "### BLUE -> GREEN ####"
   echo ">>> pull green image"
-  docker compose pull green
+  docker compose -f /home/ubuntu/docker-compose-prod.yml pull green
   echo ">>> up green container"
-  docker compose up -d green
+  docker compose -f /home/ubuntu/docker-compose-prod.yml up -d green
   while [[ 1 -eq 1 ]]; do
     echo ">>> green health check ..."
     sleep 3
-    REQUEST=$(curl -s http://127.0.0.1:8082) # -s로 결과를 깔끔히 처리
+    REQUEST=$(curl -s http://127.0.0.1:8082)
     if [[ -n "$REQUEST" ]]; then
       echo ">>> health check success !"
       break
@@ -23,19 +23,19 @@ if [[ -z "$IS_GREEN_EXIST" ]]; then
   sudo cp /etc/nginx/conf.d/green-url.inc /etc/nginx/conf.d/service-url.inc
   sudo nginx -s reload
   echo ">>> down blue container"
-  docker compose stop blue
+  docker compose -f /home/ubuntu/docker-compose-prod.yml stop blue
 
 # blue up
 else
   echo "### GREEN -> BLUE ###"
   echo ">>> pull blue image"
-  docker compose pull blue
+  docker compose -f /home/ubuntu/docker-compose-prod.yml pull blue
   echo ">>> up blue container"
-  docker compose up -d blue
+  docker compose -f /home/ubuntu/docker-compose-prod.yml up -d blue
   while [[ 1 -eq 1 ]]; do
     echo ">>> blue health check ..."
     sleep 3
-    REQUEST=$(curl -s http://127.0.0.1:8081) # -s로 결과를 깔끔히 처리
+    REQUEST=$(curl -s http://127.0.0.1:8081)
     if [[ -n "$REQUEST" ]]; then
       echo ">>> health check success !"
       break
@@ -46,5 +46,5 @@ else
   sudo cp /etc/nginx/conf.d/blue-url.inc /etc/nginx/conf.d/service-url.inc
   sudo nginx -s reload
   echo ">>> down green container"
-  docker compose stop green
+  docker compose -f /home/ubuntu/docker-compose-prod.yml stop green
 fi
