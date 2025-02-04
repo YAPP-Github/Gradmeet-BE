21987f7527098d78f8a924b94dc661d962ad76c2
jisu <dlawotn3@naver.com>
2025-02-04 12:22:26 +0900

    feat: add factory method to domain model class

--- src/main/kotlin/com/dobby/backend/domain/model/experiment/Pagination.kt
+++ src/main/kotlin/com/dobby/backend/domain/model/experiment/Pagination.kt
@@ -3,4 +3,10 @@ package com.dobby.backend.domain.model.experiment
 data class Pagination(
     val page: Int,
     val count: Int
-)
+) {
+    companion object {
+        fun newPagination(page: Int, count: Int): Pagination {
+            return Pagination(page, count)
+        }
+    }
+}
