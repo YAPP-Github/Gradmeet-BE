baedb8bdda18fdac4530456e3a7bab561c89e609
베뉴 <christer10@ewhain.net>
2025-02-05 22:06:17 +0900

    fix: fix test codes to reflect updation

--- src/test/kotlin/com/dobby/backend/application/usecase/member/VerifyContactEmailUseCaseTest.kt
+++ src/test/kotlin/com/dobby/backend/application/usecase/member/VerifyContactEmailUseCaseTest.kt
@@ -1,6 +1,6 @@
 package com.dobby.backend.application.usecase.member
 
-import com.dobby.backend.domain.exception.SignupContactEmailDuplicateException
+import com.dobby.backend.domain.exception.ContactEmailDuplicateException
 import com.dobby.backend.domain.gateway.member.MemberGateway
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
@@ -18,7 +18,7 @@ class VerifyContactEmailUseCaseTest : BehaviorSpec({
         `when`("이메일이 데이터베이스에 존재하지 않는 경우") {
             val input = VerifyContactEmailUseCase.Input(contactEmail = "newuser@example.com")
 
-            every { memberGateway.findByContactEmail(input.contactEmail) } returns null
+            every { memberGateway.existsByContactEmail(input.contactEmail) } returns false
 
             then("이메일 사용 가능 응답을 반환해야 한다") {
                 val result = verifyContactEmailUseCase.execute(input)
@@ -29,10 +29,10 @@ class VerifyContactEmailUseCaseTest : BehaviorSpec({
         `when`("이메일이 이미 존재하는 경우") {
             val input = VerifyContactEmailUseCase.Input(contactEmail = "existinguser@example.com")
 
-            every { memberGateway.findByContactEmail(input.contactEmail) } returns mockk()
+            every { memberGateway.existsByContactEmail(input.contactEmail) } returns true
 
-            then("SignupContactEmailDuplicateException 예외가 발생해야 한다") {
-                shouldThrow<SignupContactEmailDuplicateException> {
+            then("ContactEmailDuplication 예외가 발생해야 한다") {
+                shouldThrow<ContactEmailDuplicateException> {
                     verifyContactEmailUseCase.execute(input)
                 }
             }
