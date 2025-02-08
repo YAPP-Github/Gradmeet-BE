121b88ef09d7cf0249a16e24f7f47d186751199b
jisu <dlawotn3@naver.com>
2025-02-08 20:59:41 +0900

    feat: add MemberWithdrawal Entity and domain model class

---
+++ src/main/kotlin/com/dobby/backend/infrastructure/database/entity/member/MemberWithdrawalEntity.kt
@@ -0,0 +1,42 @@
+package com.dobby.backend.infrastructure.database.entity.member
+
+import com.dobby.backend.domain.model.member.MemberWithdrawal
+import com.dobby.backend.infrastructure.database.entity.enums.member.WithdrawalReasonType
+import jakarta.persistence.*
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "member_withdrawal")
+class MemberWithdrawalEntity(
+    @Id
+    @Column(name = "member_id", columnDefinition = "CHAR(13)")
+    val memberId: String,
+
+    @Column(name = "reason_type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    val reasonType: WithdrawalReasonType,
+
+    @Column(name = "other_reason", nullable = true)
+    val otherReason: String? = null,
+
+    @Column(name = "withdrawn_at", nullable = false)
+    val createdAt: LocalDateTime
+) {
+    fun toDomain() = MemberWithdrawal(
+        memberId = memberId,
+        reasonType = reasonType,
+        otherReason = otherReason,
+        createdAt = createdAt
+    )
+
+    companion object {
+        fun fromDomain(withdrawal: MemberWithdrawal) = with(withdrawal) {
+            MemberWithdrawalEntity(
+                memberId = memberId,
+                reasonType = reasonType,
+                otherReason = otherReason,
+                createdAt = createdAt
+            )
+        }
+    }
+}
