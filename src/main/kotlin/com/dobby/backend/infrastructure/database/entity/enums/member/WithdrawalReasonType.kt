5f32794bdc135124f79da9fd4e7f5a6530c56989
jisu <dlawotn3@naver.com>
2025-02-08 20:41:42 +0900

    feat: add WithdrawalReasonType enum class

---
+++ src/main/kotlin/com/dobby/backend/infrastructure/database/entity/enums/member/WithdrawalReasonType.kt
@@ -0,0 +1,12 @@
+package com.dobby.backend.infrastructure.database.entity.enums.member
+
+enum class WithdrawalReasonType(
+    val reasonType: String
+) {
+    RESEARCH_STOPPED("RESEARCH_STOPPED"),
+    SECURITY_CONCERN("SECURITY_CONCERN"),
+    NO_NECESSARY_FUNCTION("NO_NECESSARY_FUNCTION"),
+    TOO_MANY_EMAILS("TOO_MANY_EMAILS"),
+    INCONVENIENT_SITE("INCONVENIENT_SITE"),
+    OTHER("OTHER")
+}
