8105d0a5690a8bba572a08e033489ea5de22bf78
jisu <dlawotn3@naver.com>
2025-02-09 13:49:02 +0900

    refactor: decouple UseCase from DTO

--- src/main/kotlin/com/dobby/backend/application/usecase/experiment/GetExperimentPostDetailUseCase.kt
+++ src/main/kotlin/com/dobby/backend/application/usecase/experiment/GetExperimentPostDetailUseCase.kt
@@ -5,7 +5,12 @@ import com.dobby.backend.domain.exception.ExperimentPostNotFoundException
 import com.dobby.backend.domain.gateway.experiment.ExperimentPostGateway
 import com.dobby.backend.domain.model.experiment.ExperimentPost
 import com.dobby.backend.domain.model.experiment.TargetGroup
-import com.dobby.backend.presentation.api.dto.response.experiment.ExperimentPostDetailResponse
+import com.dobby.backend.infrastructure.database.entity.enums.GenderType
+import com.dobby.backend.infrastructure.database.entity.enums.MatchType
+import com.dobby.backend.infrastructure.database.entity.enums.TimeSlot
+import com.dobby.backend.infrastructure.database.entity.enums.areaInfo.Area
+import com.dobby.backend.infrastructure.database.entity.enums.areaInfo.Region
+import java.time.LocalDate
 
 class GetExperimentPostDetailUseCase(
     private val experimentPostGateway: ExperimentPostGateway
@@ -17,8 +22,47 @@ class GetExperimentPostDetailUseCase(
     )
 
     data class Output(
-        val experimentPostDetailResponse: ExperimentPostDetailResponse
+        val experimentPostDetail: ExperimentPostDetail
     )
+
+    data class ExperimentPostDetail(
+        val experimentPostId: String,
+        val title: String,
+        val uploadDate: LocalDate,
+        val uploaderName: String,
+        val views: Int,
+        val recruitStatus: Boolean,
+        val summary: Summary,
+        val targetGroup: TargetGroup,
+        val address: Address,
+        val content: String,
+        val imageList: List<String>,
+        val isAuthor: Boolean
+    ) {
+        data class Summary(
+            val startDate: LocalDate?,
+            val endDate: LocalDate?,
+            val leadResearcher: String,
+            val matchType: MatchType,
+            val reward: String,
+            val count: Int?,
+            val timeRequired: TimeSlot?
+        )
+
+        data class TargetGroup(
+            val startAge: Int?,
+            val endAge: Int?,
+            val genderType: GenderType,
+            val otherCondition: String?
+        )
+
+        data class Address(
+            val univName: String?,
+            val region: Region?,
+            val area: Area?,
+            val detailedAddress: String?
+        )
+    }
 
     override fun execute(input: Input): Output {
         val experimentPost = experimentPostGateway.findById(input.experimentPostId)
@@ -27,30 +71,30 @@ class GetExperimentPostDetailUseCase(
         experimentPostGateway.save(experimentPost)
 
         return Output(
-            experimentPostDetailResponse = experimentPost.toDetailResponse(input.memberId)
+            experimentPostDetail = experimentPost.toExperimentPostDetail(input.memberId)
         )
     }
 }
 
-fun ExperimentPost.toDetailResponse(memberId: String?): ExperimentPostDetailResponse {
-    return ExperimentPostDetailResponse(
+fun ExperimentPost.toExperimentPostDetail(memberId: String?): GetExperimentPostDetailUseCase.ExperimentPostDetail {
+    return GetExperimentPostDetailUseCase.ExperimentPostDetail(
         experimentPostId = this.id,
         title = this.title,
         uploadDate = this.createdAt.toLocalDate(),
         uploaderName = this.member.name,
         views = this.views,
         recruitStatus = this.recruitStatus,
-        summary = this.toSummaryResponse(),
-        targetGroup = this.targetGroup.toResponse(),
-        address = this.toAddressResponse(),
+        summary = this.toSummary(),
+        targetGroup = this.targetGroup.toTargetGroup(),
+        address = this.toAddress(),
         content = this.content,
         imageList = this.images.map { it.imageUrl },
         isAuthor = this.member.id == memberId
     )
 }
 
-fun ExperimentPost.toSummaryResponse(): ExperimentPostDetailResponse.SummaryResponse {
-    return ExperimentPostDetailResponse.SummaryResponse(
+fun ExperimentPost.toSummary(): GetExperimentPostDetailUseCase.ExperimentPostDetail.Summary {
+    return GetExperimentPostDetailUseCase.ExperimentPostDetail.Summary(
         startDate = this.startDate,
         endDate = this.endDate,
         leadResearcher = this.leadResearcher,
@@ -61,8 +105,8 @@ fun ExperimentPost.toSummaryResponse(): ExperimentPostDetailResponse.SummaryResp
     )
 }
 
-fun TargetGroup.toResponse(): ExperimentPostDetailResponse.TargetGroupResponse {
-    return ExperimentPostDetailResponse.TargetGroupResponse(
+fun TargetGroup.toTargetGroup(): GetExperimentPostDetailUseCase.ExperimentPostDetail.TargetGroup {
+    return GetExperimentPostDetailUseCase.ExperimentPostDetail.TargetGroup(
         startAge = this.startAge,
         endAge = this.endAge,
         genderType = this.genderType,
@@ -70,8 +114,8 @@ fun TargetGroup.toResponse(): ExperimentPostDetailResponse.TargetGroupResponse {
     )
 }
 
-fun ExperimentPost.toAddressResponse(): ExperimentPostDetailResponse.AddressResponse {
-    return ExperimentPostDetailResponse.AddressResponse(
+fun ExperimentPost.toAddress(): GetExperimentPostDetailUseCase.ExperimentPostDetail.Address {
+    return GetExperimentPostDetailUseCase.ExperimentPostDetail.Address(
         univName = this.univName,
         region = this.region,
         area = this.area,
