af4a8b8bf8a596aae6fbfd2c06c79e7e369df6d8
jisu <dlawotn3@naver.com>
2025-02-10 20:40:25 +0900

    test: add Redis Testcontainer configuration

---
+++ src/test/kotlin/com/dobby/backend/config/RedisTestContainerConfig.kt
@@ -0,0 +1,23 @@
+package com.dobby.backend.config
+
+import org.junit.jupiter.api.extension.BeforeAllCallback
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.testcontainers.containers.GenericContainer
+import org.testcontainers.utility.DockerImageName
+
+object RedisTestContainerConfig : BeforeAllCallback {
+
+    private const val REDIS_IMAGE = "redis:7.0.8-alpine"
+    private const val REDIS_PORT = 6379
+
+    private val redisContainer: GenericContainer<out GenericContainer<*>> = GenericContainer(DockerImageName.parse(REDIS_IMAGE))
+        .apply {
+            withExposedPorts(REDIS_PORT)
+            start()
+        }
+
+    override fun beforeAll(context: ExtensionContext?) {
+        System.setProperty("spring.data.redis.host", redisContainer.host)
+        System.setProperty("spring.data.redis.port", redisContainer.getMappedPort(REDIS_PORT).toString())
+    }
+}
