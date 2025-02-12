471f424c54b3622af7218657553aaa920af65dcc
베뉴 <christer10@ewhain.net>
2025-02-13 02:14:43 +0900

    fix: define request dto in presentation layer

--- src/main/kotlin/com/dobby/backend/presentation/api/dto/request/experiment/UpdateExperimentPostRequest.kt
+++ src/main/kotlin/com/dobby/backend/presentation/api/dto/request/experiment/UpdateExperimentPostRequest.kt
@@ -17,14 +17,14 @@ data class UpdateExperimentPostRequest (
     val count: Int, // N 회 참여
     val timeRequired: TimeSlot?,
 
-    val leadResearcher: String, // 연구 책임 정보 -> 기본값: 연구자 정보에서 끌어와야 함, 추후에 자유롭게 수정 가능
+    val leadResearcher: String?, // 연구 책임 정보 -> 기본값: 연구자 정보에서 끌어와야 함, 추후에 자유롭게 수정 가능
 
-    val univName: String, // 대학교 이름 -> 기본값: 연구자 정보에서 끌어와야 함, 추후에 자유롭게 수정 가능
-    val region: Region,
-    val area: Area,
+    val univName: String?, // 대학교 이름 -> 기본값: 연구자 정보에서 끌어와야 함, 추후에 자유롭게 수정 가능
+    val region: Region?,
+    val area: Area?,
     val detailedAddress: String?,
 
-    val reward: String,
-    val title: String,
-    val content: String,
+    val reward: String?,
+    val title: String?,
+    val content: String?,
 )
