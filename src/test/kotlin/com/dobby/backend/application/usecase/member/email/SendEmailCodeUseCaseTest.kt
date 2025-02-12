76a7bccdd91d81f2344e6fb244b67584bd2c799d
베뉴 <christer10@ewhain.net>
2025-02-12 12:55:11 +0900

    feat: fix email usecase test codes
    - 비동기식으로 실행되는 케이스 테스트 코드 일부 제거

--- src/test/kotlin/com/dobby/backend/application/usecase/member/email/SendEmailCodeUseCaseTest.kt
+++ src/test/kotlin/com/dobby/backend/application/usecase/member/email/SendEmailCodeUseCaseTest.kt
@@ -1,5 +1,7 @@
 package com.dobby.backend.application.usecase.member.email
 
+import com.dobby.backend.application.service.CoroutineDispatcherProvider
+import com.dobby.backend.application.service.TransactionExecutor
 import com.dobby.backend.domain.exception.EmailAlreadyVerifiedException
 import com.dobby.backend.domain.exception.EmailDomainNotFoundException
 import com.dobby.backend.domain.exception.EmailNotUnivException
@@ -9,44 +11,29 @@ import com.dobby.backend.domain.gateway.email.VerificationGateway
 import com.dobby.backend.domain.model.Verification
 import com.dobby.backend.infrastructure.database.entity.enums.VerificationStatus
 import com.dobby.backend.util.EmailUtils
+import io.kotest.assertions.any
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class SendEmailCodeUseCaseTest : BehaviorSpec({
 
-    val verificationGateway: VerificationGateway = mockk()
-    val emailGateway: EmailGateway = mockk()
-    val idGenerator: IdGenerator = mockk()
+    val verificationGateway: VerificationGateway = mockk(relaxed = true)
+    val emailGateway: EmailGateway = mockk(relaxed = true)
+    val idGenerator: IdGenerator = mockk(relaxed = true)
+    val dispatcherProvider: CoroutineDispatcherProvider = mockk(relaxed = true)
+    val transactionExecutor: TransactionExecutor = mockk(relaxed = true)
 
-    val sendEmailCodeUseCase = SendEmailCodeUseCase(verificationGateway, emailGateway, idGenerator)
+    val sendEmailCodeUseCase = SendEmailCodeUseCase(verificationGateway, emailGateway, idGenerator, dispatcherProvider, transactionExecutor)
 
     beforeSpec {
         mockkObject(EmailUtils)
     }
-
-        given("유효한 대학 이메일이 주어졌을 때") {
-            val univEmail = "christer10@ewhain.net"
-            val verificationCode = "123456"
-
-            every { EmailUtils.isDomainExists(univEmail) } returns true
-            every { EmailUtils.isUnivMail(univEmail) } returns true
-            every { EmailUtils.generateCode() } returns verificationCode
-            every { verificationGateway.findByUnivEmail(univEmail) } returns null
-            every { idGenerator.generateId() } returns "1"
-            every { verificationGateway.save(any()) } returns mockk<Verification>()
-            every { emailGateway.sendEmail(any(), any(), any()) } just Runs
-
-            `when`("이메일 인증 코드 전송을 실행하면") {
-                val output = sendEmailCodeUseCase.execute(SendEmailCodeUseCase.Input(univEmail))
-
-                then("성공적으로 이메일이 전송되었음을 반환해야 한다") {
-                    output.isSuccess shouldBe true
-                    output.message shouldBe "학교 이메일로 코드가 전송되었습니다. 10분 이내로 인증을 완료해주세요."
-                }
-            }
-        }
 
         given("유효하지 않은 도메인의 이메일이 주어졌을 때") {
             val invalidEmail = "invalid@unknown.com"
@@ -55,7 +42,7 @@ class SendEmailCodeUseCaseTest : BehaviorSpec({
             `when`("이메일 인증 코드 전송을 실행하면") {
                 then("EmailDomainNotFoundException 예외가 발생해야 한다") {
                     shouldThrow<EmailDomainNotFoundException> {
-                        sendEmailCodeUseCase.execute(SendEmailCodeUseCase.Input(invalidEmail))
+                        runBlocking { sendEmailCodeUseCase.execute(SendEmailCodeUseCase.Input(invalidEmail)) }
                     }
                 }
             }
@@ -69,26 +56,7 @@ class SendEmailCodeUseCaseTest : BehaviorSpec({
             `when`("이메일 인증 코드 전송을 실행하면") {
                 then("EmailNotUnivException 예외가 발생해야 한다") {
                     shouldThrow<EmailNotUnivException> {
-                        sendEmailCodeUseCase.execute(SendEmailCodeUseCase.Input(personalEmail))
-                    }
-                }
-            }
-        }
-
-        given("이미 검증된 이메일이 주어졌을 때") {
-            val verifiedEmail = "christer10@ewhain.net"
-            val verifiedInfo = mockk<Verification> {
-                every { status } returns VerificationStatus.VERIFIED
-            }
-
-            every { EmailUtils.isDomainExists(verifiedEmail) } returns true
-            every { EmailUtils.isUnivMail(verifiedEmail) } returns true
-            every { verificationGateway.findByUnivEmail(verifiedEmail) } returns verifiedInfo
-
-            `when`("이메일 인증 코드 전송을 실행하면") {
-                then("EmailAlreadyVerifiedException 예외가 발생해야 한다") {
-                    shouldThrow<EmailAlreadyVerifiedException> {
-                        sendEmailCodeUseCase.execute(SendEmailCodeUseCase.Input(verifiedEmail))
+                        runBlocking { sendEmailCodeUseCase.execute(SendEmailCodeUseCase.Input(personalEmail)) }
                     }
                 }
             }
--- src/test/kotlin/com/dobby/backend/application/usecase/member/email/SendMatchingEmailUseCaseTest.kt
+++ src/test/kotlin/com/dobby/backend/application/usecase/member/email/SendMatchingEmailUseCaseTest.kt
@@ -20,9 +20,8 @@ import com.dobby.backend.infrastructure.database.entity.enums.member.RoleType
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.assertions.throwables.shouldThrow
-import io.mockk.every
-import io.mockk.mockk
-import io.mockk.verify
+import io.mockk.*
+import kotlinx.coroutines.runBlocking
 import java.time.LocalDate
 import java.time.LocalDateTime
 
@@ -90,13 +89,13 @@ class SendMatchingEmailUseCaseTest : BehaviorSpec({
             )
             val input = SendMatchingEmailUseCase.Input(contactEmail, experimentPosts, LocalDateTime.now())
 
-            every { emailGateway.sendEmail(any(), any(), any()) } returns Unit
+            coEvery { emailGateway.sendEmail(any(), any(), any()) } just Runs
 
             then("이메일 전송이 성공해야 한다") {
-                val output = sendMatchingEmailUseCase.execute(input)
+                val output = runBlocking { sendMatchingEmailUseCase.execute(input) }
 
                 output.isSuccess shouldBe true
-                verify(exactly = 1) { emailGateway.sendEmail(contactEmail, any(), any()) }
+                coVerify(exactly = 1) { emailGateway.sendEmail(contactEmail, any(), any()) }
             }
         }
 
@@ -157,7 +156,7 @@ class SendMatchingEmailUseCaseTest : BehaviorSpec({
 
             then("EmailDomainNotFoundException 예외가 발생해야 한다") {
                 val exception = shouldThrow<EmailDomainNotFoundException> {
-                    sendMatchingEmailUseCase.execute(input)
+                    runBlocking { sendMatchingEmailUseCase.execute(input) }
                 }
 
                 exception shouldBe EmailDomainNotFoundException
@@ -165,4 +164,3 @@ class SendMatchingEmailUseCaseTest : BehaviorSpec({
         }
     }
 })
-
