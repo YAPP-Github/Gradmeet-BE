2d148251864b10599d9ea2c44176f713eb952bad
베뉴 <christer10@ewhain.net>
2025-02-16 01:41:57 +0900

    feat: modify useCases for memberInfo select and update
    - 유즈케이스에서 추가된 필드에 따라 업데이트 로직을 수정했습니다.
    - 커스텀 예외 로직을 적용했습니다.

--- src/main/kotlin/com/dobby/backend/application/usecase/member/UpdateResearcherInfoUseCase.kt
+++ src/main/kotlin/com/dobby/backend/application/usecase/member/UpdateResearcherInfoUseCase.kt
@@ -2,14 +2,18 @@ package com.dobby.backend.application.usecase.member
 
 import com.dobby.backend.application.usecase.UseCase
 import com.dobby.backend.domain.exception.ContactEmailDuplicateException
+import com.dobby.backend.domain.exception.MemberConsentNotFoundException
 import com.dobby.backend.domain.exception.ResearcherNotFoundException
+import com.dobby.backend.domain.gateway.member.MemberConsentGateway
 import com.dobby.backend.domain.gateway.member.MemberGateway
 import com.dobby.backend.domain.gateway.member.ResearcherGateway
 import com.dobby.backend.domain.model.member.Member
+import io.swagger.v3.oas.annotations.media.Schema
 
 class UpdateResearcherInfoUseCase(
     private val researcherGateway: ResearcherGateway,
-    private val memberGateway: MemberGateway
+    private val memberGateway: MemberGateway,
+    private val memberConsentGateway: MemberConsentGateway
 ) : UseCase<UpdateResearcherInfoUseCase.Input, UpdateResearcherInfoUseCase.Output> {
     data class Input(
         val memberId: String,
@@ -17,7 +21,8 @@ class UpdateResearcherInfoUseCase(
         val name: String,
         val univName: String,
         val major: String,
-        val labInfo: String?
+        val labInfo: String?,
+        var adConsent: Boolean,
     )
 
     data class Output(
@@ -25,13 +30,16 @@ class UpdateResearcherInfoUseCase(
         val univEmail: String,
         val univName: String,
         val major: String,
-        val labInfo: String?
+        val labInfo: String?,
+        val adConsent: Boolean
     )
 
     override fun execute(input: Input): Output {
         val researcher = researcherGateway.findByMemberId(input.memberId)
             ?: throw ResearcherNotFoundException
-        if (memberGateway.existsByContactEmail(input.contactEmail)&& researcher.member.contactEmail != input.contactEmail) {
+        val researcherConsent = memberConsentGateway.findByMemberId(input.memberId)
+            ?: throw MemberConsentNotFoundException
+        if (memberGateway.existsByContactEmail(input.contactEmail) && researcher.member.contactEmail != input.contactEmail) {
             throw ContactEmailDuplicateException
         }
 
@@ -45,12 +53,21 @@ class UpdateResearcherInfoUseCase(
             )
         )
 
+        val updatedConsent = memberConsentGateway.save(
+            researcherConsent.update(
+                memberId = updatedResearcher.member.id,
+                adConsent = input.adConsent,
+                matchConsent = false
+            )
+        )
+
         return Output(
             member = updatedResearcher.member,
             univEmail = updatedResearcher.univEmail,
             univName = updatedResearcher.univName,
             major = updatedResearcher.major,
-            labInfo = updatedResearcher.labInfo
+            labInfo = updatedResearcher.labInfo,
+            adConsent = updatedConsent.adConsent
         )
     }
 }
