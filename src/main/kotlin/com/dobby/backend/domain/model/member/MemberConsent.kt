a1a2554c4d66d9735b2dad87a7209809a77f7f0f
베뉴 <christer10@ewhain.net>
2025-02-16 00:29:17 +0900

    feat: define MemberConsent entity and domain model
    - 회원 이용약관 동의 정보를 관리하기 위한 DB 엔티티, 도메인 모델 정의

---
+++ src/main/kotlin/com/dobby/backend/domain/model/member/MemberConsent.kt
@@ -0,0 +1,35 @@
+package com.dobby.backend.domain.model.member
+
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+data class MemberConsent(
+    val memberId: String,
+    val adConsent: Boolean,
+    val matchConsent: Boolean,
+    val adConsentedAt: LocalDate?,
+    val matchConsentedAt: LocalDate?,
+    val createdAt: LocalDateTime,
+    val updatedAt: LocalDateTime,
+) {
+    companion object {
+        fun newConsent(
+            memberId: String,
+            adConsent: Boolean,
+            matchConsent: Boolean,
+            createdAt: LocalDateTime = LocalDateTime.now()
+        ) : MemberConsent {
+            val nowDate = createdAt.toLocalDate()
+
+            return MemberConsent(
+                memberId = memberId,
+                adConsent = adConsent,
+                matchConsent = matchConsent,
+                adConsentedAt = if (adConsent) nowDate else null,
+                matchConsentedAt = if (matchConsent) nowDate else null,
+                createdAt = createdAt,
+                updatedAt = createdAt
+            )
+        }
+    }
+}
