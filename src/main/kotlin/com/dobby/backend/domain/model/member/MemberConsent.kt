01f1b5cc97bb7faafc3ef1462ae93c858e3a2641
베뉴 <christer10@ewhain.net>
2025-02-16 16:29:10 +0900

    refact: improve readability by replacing if statements with when for update method

--- src/main/kotlin/com/dobby/backend/domain/model/member/MemberConsent.kt
+++ src/main/kotlin/com/dobby/backend/domain/model/member/MemberConsent.kt
@@ -41,12 +41,16 @@ data class MemberConsent(
         return this.copy(
             adConsent = adConsent,
             matchConsent = matchConsent,
-            adConsentedAt = if (adConsent != this.adConsent) {
-                if(adConsent) nowDate else null
-            } else { this.adConsentedAt },
-            matchConsentedAt = if(matchConsent != this.matchConsent) {
-                if(matchConsent) nowDate else null
-            } else { this.matchConsentedAt },
+            adConsentedAt = when {
+                adConsent == this.adConsent -> this.adConsentedAt
+                adConsent -> nowDate
+                else -> null
+            },
+            matchConsentedAt = when {
+                matchConsent == this.matchConsent -> this.matchConsentedAt
+                matchConsent -> nowDate
+                else -> null
+            },
             updatedAt = updatedAt
         )
     }
