2e5fbe369b89ea0885de7a7b39459c331bb12af2
베뉴 <christer10@ewhain.net>
2025-02-16 13:26:35 +0900

    feat: add `code` with TTL and rate limiting in cache layer
    - 하루에 최대 3번 인증 코드 전송 요청 가능→ 처리율 제한 로직 구현
    - 인증 코드를 Redis 캐시 관리 방식으로 위임

--- src/main/kotlin/com/dobby/backend/infrastructure/gateway/cache/RedisCacheGatewayImpl.kt
+++ src/main/kotlin/com/dobby/backend/infrastructure/gateway/cache/RedisCacheGatewayImpl.kt
@@ -13,6 +13,8 @@ class RedisCacheGatewayImpl(
 ) : CacheGateway {
 
     private val cacheTimeout = 240L
+    private val codeTimeout = 10L
+    private val requestTimeout = 24L
 
     override fun get(key: String): String? {
         return redisTemplate.opsForValue().get(getCacheKey(key))
@@ -22,8 +24,21 @@ class RedisCacheGatewayImpl(
         redisTemplate.opsForValue().set(getCacheKey(key), value, cacheTimeout, TimeUnit.MINUTES)
     }
 
+    override fun setCode(key: String, value: String) {
+        redisTemplate.opsForValue().set(getCacheKey(key), value, codeTimeout, TimeUnit.MINUTES)
+    }
+
     override fun evict(key: String) {
         redisTemplate.delete(getCacheKey(key))
+    }
+
+    override fun incrementRequestCount(key: String) {
+        val cacheKey = getCacheKey(key)
+        val count = redisTemplate.opsForValue().increment(cacheKey, 1) ?: 1
+
+        if(count == 1L){
+            redisTemplate.expire(cacheKey, requestTimeout, TimeUnit.HOURS)
+        }
     }
 
     private fun getCacheKey(key: String): String {
