66531fea5a45b99e41905b60536968f0eed227b5
Jisu Lim <69844138+Ji-soo708@users.noreply.github.com>
2025-02-20 00:56:47 +0900

    [YS-345] feat: 데모데이를 위한 회원 매칭 이메일 동의 여부 조작 API 구현 (#112)
    * feat: add updateMatchConsent api for demo-day
    
    * style: add TODO (update variable to 'val' after Demo Day)
    
    * refactor: refactor updateMatchConsent

--- src/main/kotlin/com/dobby/backend/application/service/SchedulerService.kt
+++ src/main/kotlin/com/dobby/backend/application/service/SchedulerService.kt
@@ -1,15 +1,27 @@
 package com.dobby.backend.application.service
 
 import com.dobby.backend.application.usecase.quartz.TriggerSchedulerUseCase
+import com.dobby.backend.domain.exception.MemberNotFoundException
+import com.dobby.backend.domain.gateway.member.MemberConsentGateway
+import com.dobby.backend.domain.gateway.member.MemberGateway
 import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
 
 @Service
 class SchedulerService(
-    private val schedulerTriggerUseCase: TriggerSchedulerUseCase
+    private val schedulerTriggerUseCase: TriggerSchedulerUseCase,
+    private val memberConsentGateway: MemberConsentGateway,
+    private val memberGateway: MemberGateway
 ) {
     @Transactional
     fun triggerSendMatchingEmailJob() {
         schedulerTriggerUseCase.execute(TriggerSchedulerUseCase.Input("matching_email_send_job"))
+    }
+
+    @Transactional
+    fun updateMemberConsent(name: String) {
+        val memberId = memberGateway.findMemberIdByName(name)
+            ?: throw MemberNotFoundException
+        memberConsentGateway.updateMatchConsent(memberId, false)
     }
 }
