aa01c6dd7134973529760414c05c3de4292c93ea
Jisu Lim <69844138+Ji-soo708@users.noreply.github.com>
2025-02-19 20:03:35 +0900

    [YS-344] hotfix: S3 SDK v1 → v2 버전 업그레이드 (#111)
    * chore: update S3 to use AWS SDK v2
    
    * refactor: delete unused gateway

--- src/main/kotlin/com/dobby/backend/domain/gateway/experiment/TargetGroupGateway.kt
+++
@@ -1,7 +0,0 @@
-package com.dobby.backend.domain.gateway.experiment
-
-import com.amazonaws.services.ec2.model.TargetGroup
-
-interface TargetGroupGateway {
-    fun save(targetGroup: TargetGroup): TargetGroup
-}
