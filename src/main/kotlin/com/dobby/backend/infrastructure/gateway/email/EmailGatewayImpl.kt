a287f45fea00f11066574db7c699f6a73fc63d06
베뉴 <christer10@ewhain.net>
2025-02-25 17:56:37 +0900

    refactor: revise the send email implement code to meet the ses client

--- src/main/kotlin/com/dobby/backend/infrastructure/gateway/email/EmailGatewayImpl.kt
+++ src/main/kotlin/com/dobby/backend/infrastructure/gateway/email/EmailGatewayImpl.kt
@@ -1,35 +1,45 @@
 package com.dobby.backend.infrastructure.gateway.email
 
 import com.dobby.backend.domain.gateway.email.EmailGateway
-import com.dobby.backend.infrastructure.config.properties.EmailProperties
+import com.dobby.backend.infrastructure.config.properties.SESProperties
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import org.springframework.mail.SimpleMailMessage
-import org.springframework.mail.javamail.JavaMailSender
-import org.springframework.mail.javamail.MimeMessageHelper
 import org.springframework.stereotype.Component
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.ses.SesClient
+import software.amazon.awssdk.services.ses.model.*
 
 @Component
 class EmailGatewayImpl(
-    private val emailProperties: EmailProperties,
-    private val mailSender: JavaMailSender
+    private val sesClient: SesClient,
+    private val sesProperties: SESProperties
 ) : EmailGateway {
 
     override suspend fun sendEmail(to: String, subject: String, content: String, isHtml: Boolean) {
         withContext(Dispatchers.IO) {
             try {
-                val message = mailSender.createMimeMessage()
-                val helper = MimeMessageHelper(message, false, "UTF-8")
+                val body = if(isHtml) {
+                    Body.builder().html(Content.builder().data(content).build()).build()
+                } else {
+                    Body.builder().text(Content.builder().data(content).build()).build()
+                }
 
-                helper.setTo(to)
-                helper.setSubject(subject)
-                helper.setText(content, isHtml)
+                val request = SendEmailRequest.builder()
+                    .source(sesProperties.email.sender)
+                    .destination(Destination.builder().toAddresses(to).build())
+                    .message(
+                        Message.builder()
+                            .subject(Content.builder().data(subject).build())
+                            .body(body)
+                            .build()
+                    )
+                    .build()
 
-                mailSender.send(message)
+                sesClient.sendEmail(request)
+
             } catch (ex: Exception) {
                 throw IllegalStateException("이메일 발송 실패: ${ex.message}")
             }
         }
     }
 }
-
