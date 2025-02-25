39f2e06b09711b42e4a1107d0a736b0a18c3eb51
Sujung Shin <113707388+chock-cho@users.noreply.github.com>
2025-02-25 23:52:26 +0900

    [YS-330] refactor: 이메일 전송 기능 AWS SES로 마이그레이션 (#121)
    * feat: add aws ses sdk dependency
    
    * feat: define SES configuration files in infrastructure layer
    
    * refactor: revise the send email implement code to meet the ses client
    
    * fix: correct aws spring cloud dependency typo
    
    * test: add missing ses properties in test env
    
    * fix: delete unused smtp config in test env
    
    * refactor: delete unused email properties file
    
    * refactor: replace SesClient with SesAsyncClient and refact to
    whenComplete
    
    - 기존 `SesClient`를 `SesAsyncClient`로 리팩터링하여 비동기 전송
      방식으로 개선
    - `whenComplete`를 활용하여 AWS SDK의 `CompletableFeature` 최적화
    - `SesException`과 일반 예외를 구분하여 서드 파티 예외처리를 명확하게
      개선

--- src/main/kotlin/com/dobby/backend/infrastructure/gateway/email/EmailGatewayImpl.kt
+++ src/main/kotlin/com/dobby/backend/infrastructure/gateway/email/EmailGatewayImpl.kt
@@ -1,35 +1,47 @@
 package com.dobby.backend.infrastructure.gateway.email
 
 import com.dobby.backend.domain.gateway.email.EmailGateway
-import com.dobby.backend.infrastructure.config.properties.EmailProperties
+import com.dobby.backend.infrastructure.config.properties.SESProperties
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.future.await
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import org.springframework.mail.SimpleMailMessage
-import org.springframework.mail.javamail.JavaMailSender
-import org.springframework.mail.javamail.MimeMessageHelper
 import org.springframework.stereotype.Component
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.ses.SesAsyncClient
+import software.amazon.awssdk.services.ses.SesClient
+import software.amazon.awssdk.services.ses.model.*
 
 @Component
 class EmailGatewayImpl(
-    private val emailProperties: EmailProperties,
-    private val mailSender: JavaMailSender
+    private val sesAsyncClient: SesAsyncClient,
+    private val sesProperties: SESProperties
 ) : EmailGateway {
 
     override suspend fun sendEmail(to: String, subject: String, content: String, isHtml: Boolean) {
-        withContext(Dispatchers.IO) {
-            try {
-                val message = mailSender.createMimeMessage()
-                val helper = MimeMessageHelper(message, false, "UTF-8")
-
-                helper.setTo(to)
-                helper.setSubject(subject)
-                helper.setText(content, isHtml)
-
-                mailSender.send(message)
-            } catch (ex: Exception) {
-                throw IllegalStateException("이메일 발송 실패: ${ex.message}")
-            }
+        val body = if(isHtml) {
+            Body.builder().html(Content.builder().data(content).build()).build()
+        } else {
+            Body.builder().text(Content.builder().data(content).build()).build()
         }
+
+        val request = SendEmailRequest.builder()
+            .source(sesProperties.email.sender)
+            .destination(Destination.builder().toAddresses(to).build())
+            .message(
+                Message.builder()
+                    .subject(Content.builder().data(subject).build())
+                    .body(body)
+                    .build()
+            )
+            .build()
+
+        sesAsyncClient.sendEmail(request)
+            .whenComplete { _, ex ->
+                when(ex){
+                    is SesException -> throw IllegalStateException("AWS SES 오류 발생: ${ex.awsErrorDetails()?.errorMessage()}")
+                    is Exception -> throw IllegalStateException("이메일 전송 실패: ${ex.message}")                }
+            }
     }
 }
-
