f306433ba8f277ce79d661a202655c6f1d9cc4fc
Sujung Shin <113707388+chock-cho@users.noreply.github.com>
2025-02-25 10:52:12 +0900

    [YS-352] refactor: 학교 메일 인증을 완료했으나 회원가입을 마치지 않은 사용자를 구분하는 로직 추가 (#119)
    * feat: define exception to distinguish verified-but-didn't-signup-yet usecase
    
    * feat: add existsByUnivEmail method to distinguish exception
    
    * feat: add exception logic for sending email code usecase
    
    * test: add test codes for newly added exception
    
    * fix: activate rate limit logic and delete its code comments
    
    * fix: fix typo to meet the exception convention rules

--- src/test/kotlin/com/dobby/backend/application/usecase/member/email/SendEmailCodeUseCaseTest.kt
+++ src/test/kotlin/com/dobby/backend/application/usecase/member/email/SendEmailCodeUseCaseTest.kt
@@ -3,12 +3,13 @@ package com.dobby.backend.application.usecase.member.email
 import com.dobby.backend.application.common.CoroutineDispatcherProvider
 import com.dobby.backend.application.common.TransactionExecutor
 import com.dobby.backend.domain.EmailTemplateLoader
-import com.dobby.backend.domain.exception.EmailDomainNotFoundException
-import com.dobby.backend.domain.exception.EmailNotUnivException
 import com.dobby.backend.domain.IdGenerator
+import com.dobby.backend.domain.enums.VerificationStatus
+import com.dobby.backend.domain.exception.*
 import com.dobby.backend.domain.gateway.CacheGateway
 import com.dobby.backend.domain.gateway.email.EmailGateway
 import com.dobby.backend.domain.gateway.email.VerificationGateway
+import com.dobby.backend.domain.gateway.member.ResearcherGateway
 import com.dobby.backend.util.EmailUtils
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
@@ -18,6 +19,7 @@ import kotlinx.coroutines.test.runTest
 class SendEmailCodeUseCaseTest : BehaviorSpec({
 
     val verificationGateway: VerificationGateway = mockk(relaxed = true)
+    val researcherGateway: ResearcherGateway = mockk(relaxed = true)
     val emailGateway: EmailGateway = mockk(relaxed = true)
     val cacheGateway: CacheGateway = mockk(relaxed = true)
     val idGenerator: IdGenerator = mockk(relaxed = true)
@@ -25,10 +27,14 @@ class SendEmailCodeUseCaseTest : BehaviorSpec({
     val transactionExecutor: TransactionExecutor = mockk(relaxed = true)
     val emailTemplateLoader: EmailTemplateLoader = mockk(relaxed = true)
 
-    val sendEmailCodeUseCase = SendEmailCodeUseCase(verificationGateway, emailGateway, cacheGateway, idGenerator, dispatcherProvider, transactionExecutor, emailTemplateLoader)
+    val sendEmailCodeUseCase = SendEmailCodeUseCase(verificationGateway, researcherGateway, emailGateway, cacheGateway, idGenerator, dispatcherProvider, transactionExecutor, emailTemplateLoader)
 
     beforeSpec {
         mockkObject(EmailUtils)
+    }
+
+    afterSpec {
+        unmockkObject(EmailUtils)
     }
 
     given("유효하지 않은 도메인의 이메일이 주어졌을 때") {
@@ -56,6 +62,60 @@ class SendEmailCodeUseCaseTest : BehaviorSpec({
                 runTest {
                     shouldThrow<EmailNotUnivException> {
                         sendEmailCodeUseCase.execute(SendEmailCodeUseCase.Input(personalEmail))
+                    }
+                }
+            }
+        }
+    }
+
+    given("이미 가입된 대학 이메일이 주어졌을 때") {
+        val duplicateEmail = "christer10@ewhain.net"
+        every { EmailUtils.isDomainExists(duplicateEmail) } returns true
+        every { EmailUtils.isUnivMail(duplicateEmail) } returns true
+        every { researcherGateway.existsByUnivEmail(duplicateEmail) } returns true
+
+        `when`("이메일 인증 코드 전송을 실행하면") {
+            then("SignupUnivEmailDuplicateException 예외가 발생해야 한다") {
+                runTest {
+                    shouldThrow<SignupUnivEmailDuplicateException> {
+                        sendEmailCodeUseCase.execute(SendEmailCodeUseCase.Input(duplicateEmail))
+                    }
+                }
+            }
+        }
+    }
+
+    given("회원가입은 완료하지 않았지만, 이미 인증된 대학 이메일이 주어졌을 때") {
+        val verifiedEmail = "verified@ewhain.net"
+        every { EmailUtils.isDomainExists(verifiedEmail) } returns true
+        every { EmailUtils.isUnivMail(verifiedEmail) } returns true
+        every { researcherGateway.existsByUnivEmail(verifiedEmail) } returns false
+        every { verificationGateway.findByUnivEmailAndStatus(verifiedEmail, VerificationStatus.VERIFIED) } returns mockk()
+
+        `when`("이메일 인증 코드 전송을 실행하면") {
+            then("EmailAlreadyVerifiedException 예외가 발생해야 한다") {
+                runTest {
+                    shouldThrow<EmailAlreadyVerifiedException> {
+                        sendEmailCodeUseCase.execute(SendEmailCodeUseCase.Input(verifiedEmail))
+                    }
+                }
+            }
+        }
+    }
+
+    given("이메일 인증 코드 요청 횟수가 초과된 경우") {
+        val limitedEmail = "limited@ewhain.net"
+        every { EmailUtils.isDomainExists(limitedEmail) } returns true
+        every { EmailUtils.isUnivMail(limitedEmail) } returns true
+        every { researcherGateway.existsByUnivEmail(limitedEmail) } returns false
+        every { verificationGateway.findByUnivEmailAndStatus(limitedEmail, VerificationStatus.VERIFIED) } returns null
+        every { cacheGateway.get("request_count:$limitedEmail") } returns "3"
+
+        `when`("이메일 인증 코드 전송을 실행하면") {
+            then("TooManyVerificationRequestException 예외가 발생해야 한다") {
+                runTest {
+                    shouldThrow<TooManyVerificationRequestException> {
+                        sendEmailCodeUseCase.execute(SendEmailCodeUseCase.Input(limitedEmail))
                     }
                 }
             }
