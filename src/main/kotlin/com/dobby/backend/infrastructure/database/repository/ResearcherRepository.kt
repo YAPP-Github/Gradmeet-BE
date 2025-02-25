e1a026be9b0321e606a780f92ce2b22d68e774ef
베뉴 <christer10@ewhain.net>
2025-02-25 10:14:19 +0900

    feat: add existsByUnivEmail method to distinguish exception

--- src/main/kotlin/com/dobby/backend/infrastructure/database/repository/ResearcherRepository.kt
+++ src/main/kotlin/com/dobby/backend/infrastructure/database/repository/ResearcherRepository.kt
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 interface ResearcherRepository: JpaRepository<ResearcherEntity, String> {
     fun findByMemberId(memberId: String) : ResearcherEntity?
     fun findByMemberIdAndMemberDeletedAtIsNull(memberId: String): ResearcherEntity?
+    fun existsByUnivEmail(univEmail: String): Boolean
 }
