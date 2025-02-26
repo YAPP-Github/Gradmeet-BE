f741a5e1374a376cccd3f9287cf59c474cf3244a
베뉴 <christer10@ewhain.net>
2025-02-27 02:30:55 +0900

    refactor: remove coroutine infrastructure and related configurations

--- src/main/kotlin/com/dobby/backend/application/common/CoroutineDispatcherProvider.kt
+++
@@ -1,8 +0,0 @@
-package com.dobby.backend.application.common
-
-import kotlinx.coroutines.CoroutineDispatcher
-
-interface CoroutineDispatcherProvider {
-    val io: CoroutineDispatcher
-    val default: CoroutineDispatcher
-}
