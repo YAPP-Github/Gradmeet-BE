9854b98e46b9b2f3f9bb279a5e3595b58f16dd59
Jisu Lim <69844138+Ji-soo708@users.noreply.github.com>
2025-03-04 20:23:25 +0900

    [YS-382] test: 멀티모듈 전환 중 누락된 infrastructure 테스트 코드 추가 (#129)
    * feature: add basic Spring Boot test configuration
    
    * chore: add test library to build.gradle.kts
    
    * test: add test code for infrastructure module
    
    * chore : add mockk library

---
+++ infrastructure/src/test/kotlin/com/dobby/DobbyBackendApplicationTests.kt
@@ -0,0 +1,14 @@
+package com.dobby
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootTest
+@ActiveProfiles("test")
+class DobbyBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
