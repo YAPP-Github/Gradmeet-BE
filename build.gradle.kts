37d2b7d0aff9d255524364c5e8f57d6183fc2bba
베뉴 <christer10@ewhain.net>
2025-03-24 21:09:51 +0900

    chore: update root project build.gradle with only essential dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,81 +1,24 @@
 plugins {
-	kotlin("jvm") version "1.9.25"
-	kotlin("plugin.spring") version "1.9.25"
-	kotlin("plugin.jpa") version "1.9.25"
-	kotlin("kapt") version "1.9.25"
-	id("org.springframework.boot") version "3.4.1"
-	id("io.spring.dependency-management") version "1.1.7"
+    kotlin("jvm") version "1.9.25" apply false
+    kotlin("plugin.spring") version "1.9.25" apply false
+    kotlin("plugin.jpa") version "1.9.25" apply false
+    kotlin("kapt") version "1.9.25" apply false
+    id("org.springframework.boot") version "3.4.1" apply false
+    id("io.spring.dependency-management") version "1.1.7" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.0" apply false
 }
 
 group = "com.dobby"
 version = "0.0.1-SNAPSHOT"
 
-java {
-	toolchain {
-		languageVersion.set(JavaLanguageVersion.of(17))
-	}
+allprojects {
+    repositories {
+        mavenCentral()
+    }
 }
 
-tasks.getByName<Jar>("jar") {
-	enabled = true
-}
-
-
-repositories {
-	mavenCentral()
-}
-
-dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.springframework.boot:spring-boot-starter-mail")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.7.3")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.mariadb.jdbc:mariadb-java-client:2.7.3")
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.7.0")
-	implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
-	implementation("org.springframework.boot:spring-boot-starter-quartz")
-	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-	implementation("io.awspring.cloud:spring-cloud-starter-aws:2.4.4")
-	implementation("software.amazon.awssdk:s3:2.20.59")
-	implementation("software.amazon.awssdk:ses:2.20.100")
-	implementation("org.springframework.boot:spring-boot-starter-data-redis")
-	implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.3")
-	implementation("org.springframework.boot:spring-boot-starter-aop")
-	implementation("ca.pjer:logback-awslogs-appender:1.6.0")
-
-	compileOnly("org.projectlombok:lombok")
-	runtimeOnly("com.mysql:mysql-connector-j")
-	runtimeOnly("com.h2database:h2")
-	annotationProcessor("org.projectlombok:lombok")
-}
-
-dependencyManagement {
-	imports {
-		mavenBom("org.springframework.cloud:spring-cloud-dependencies:2024.0.0")
-	}
-}
-
-dependencies {
-	implementation("org.slf4j:slf4j-api")
-	implementation("ch.qos.logback:logback-classic")
-}
-
-kotlin {
-	compilerOptions {
-		freeCompilerArgs.addAll("-Xjsr305=strict")
-	}
-}
-
-tasks.getByName<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
-	enabled = false
-}
-
-tasks.getByName<Jar>("jar") {
-	enabled = true
+subprojects {
+    plugins.withId("org.jetbrains.kotlin.jvm") {
+        apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    }
 }
