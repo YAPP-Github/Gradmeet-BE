4c647447406c881bb2fc852c42de7a5759693d11
베뉴 <christer10@ewhain.net>
2025-03-29 15:14:38 +0900

    chore: clean up root build config
    - 루트 build.gradle.kts 에서 모든 서브모듈에 공통 적용할 필요 없는
      플러그인 선언을 제거하고, essential한 설정만 남김

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,3 @@
-plugins {
-    kotlin("jvm") version "1.9.25" apply false
-    kotlin("plugin.spring") version "1.9.25" apply false
-    kotlin("plugin.jpa") version "1.9.25" apply false
-    kotlin("kapt") version "1.9.25" apply false
-    id("org.springframework.boot") version "3.4.1" apply false
-    id("io.spring.dependency-management") version "1.1.7" apply false
-    id("org.jlleitschuh.gradle.ktlint") version "10.2.0" apply false
-}
-
-group = "com.dobby"
-version = "0.0.1-SNAPSHOT"
-
 allprojects {
     repositories {
         mavenCentral()
