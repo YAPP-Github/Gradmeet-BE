19b8c849e345c789397a76e0b528bd9bcf9dec23
베뉴 <christer10@ewhain.net>
2025-03-29 16:06:59 +0900

    refactor: centralize plugin versions via pluginManagement in settings.gradle
    - 각 모듈의 빌드 스크립트에 흩어져있던 플러그인 버전관리를 settings.gradle의
      pluginManagement로 이동
    - 플러그인 버전 일관성

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,5 @@
-allprojects {
+subprojects {
     repositories {
         mavenCentral()
-    }
-}
-
-subprojects {
-    plugins.withId("org.jetbrains.kotlin.jvm") {
-        apply(plugin = "org.jlleitschuh.gradle.ktlint")
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,19 @@
 rootProject.name = "dobby-backend"
 
 include("domain", "application", "presentation", "infrastructure", "common")
+
+pluginManagement {
+    plugins {
+        id("org.jetbrains.kotlin.jvm") version "1.9.25"
+        id("org.jetbrains.kotlin.plugin.spring") version "1.9.25"
+        id("org.jetbrains.kotlin.plugin.jpa") version "1.9.25"
+        id("org.jetbrains.kotlin.kapt") version "1.9.25"
+        id("org.springframework.boot") version "3.4.1"
+        id("io.spring.dependency-management") version "1.1.7"
+        id("org.jlleitschuh.gradle.ktlint") version "11.6.1"
+    }
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
