66d9fc76e9ce12cc821e1d0931a4e36cec359ff7
Jisu Lim <69844138+Ji-soo708@users.noreply.github.com>
2025-06-07 22:22:23 +0900

    Revert "[YS-456] feat: OAuth 로그인 API의 redirect URI 동적 처리 (#140)"
    This reverts commit 93b1cce20510519c025950e1a4a9a338a7b1f782.

--- domain/src/main/kotlin/com/dobby/gateway/auth/GoogleAuthGateway.kt
+++ domain/src/main/kotlin/com/dobby/gateway/auth/GoogleAuthGateway.kt
@@ -4,11 +4,6 @@ import com.dobby.model.auth.GoogleToken
 import com.dobby.model.auth.GoogleUserInfo
 
 interface GoogleAuthGateway {
-
-    fun getAccessToken(
-        code: String,
-        redirectUri: String
-    ): GoogleToken
-
+    fun getAccessToken(code: String): GoogleToken
     fun getUserInfo(accessToken: String): GoogleUserInfo
 }
