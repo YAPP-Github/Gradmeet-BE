9365261549d75de804b46a820be915c23a38c581
베뉴 <christer10@ewhain.net>
2025-07-13 01:29:50 +0900

    feature: add SearchUniversityAutoCompleteUseCase class

---
+++ application/src/main/kotlin/com/dobby/usecase/member/SearchUniversityAutoCompleteUseCase.kt
@@ -0,0 +1,23 @@
+package com.dobby.usecase.member
+
+import com.dobby.enums.University
+import com.dobby.usecase.UseCase
+
+class SearchUniversityAutoCompleteUseCase(
+) : UseCase<SearchUniversityAutoCompleteUseCase.Input, SearchUniversityAutoCompleteUseCase.Output> {
+
+    data class Input(
+        val query: String
+    )
+
+    data class Output(
+        val output: List<University>
+    )
+
+    @Override
+    override fun execute(input: Input): Output {
+        return Output(
+            output = University.match(input.query)
+        )
+    }
+}
