08f77be3b663cc9536f97f18bf22ab5f85dd79b4
베뉴 <christer10@ewhain.net>
2025-07-13 17:34:16 +0900

    feature: add cache config to usecase class layer

--- application/src/main/kotlin/com/dobby/usecase/member/SearchUniversityAutoCompleteUseCase.kt
+++ application/src/main/kotlin/com/dobby/usecase/member/SearchUniversityAutoCompleteUseCase.kt
@@ -1,9 +1,12 @@
 package com.dobby.usecase.member
 
 import com.dobby.enums.University
+import com.dobby.gateway.CacheGateway
 import com.dobby.usecase.UseCase
 
-class SearchUniversityAutoCompleteUseCase() : UseCase<SearchUniversityAutoCompleteUseCase.Input, SearchUniversityAutoCompleteUseCase.Output> {
+class SearchUniversityAutoCompleteUseCase(
+    private val cacheGateway: CacheGateway
+) : UseCase<SearchUniversityAutoCompleteUseCase.Input, SearchUniversityAutoCompleteUseCase.Output> {
 
     data class Input(
         val query: String
@@ -14,8 +17,29 @@ class SearchUniversityAutoCompleteUseCase() : UseCase<SearchUniversityAutoComple
     )
 
     override fun execute(input: Input): Output {
+        val cleanedQuery = input.query.clean()
+        val cacheKey = "autocomplete:$cleanedQuery"
+
+        cacheGateway.getAutoComplete(cacheKey)?.let {
+            return Output(it)
+        }
+
+        val result = University.match(cleanedQuery)
+
+        cacheGateway.setAutoComplete(cacheKey, result)
         return Output(
-            output = University.match(input.query)
+            output = result
         )
+    }
+
+    private fun String.clean(): String {
+        val suffixes = listOf("대학교", "캠퍼스")
+
+        for (suffix in suffixes) {
+            if (this.endsWith(suffix)) {
+                return this.removeSuffix(suffix)
+            }
+        }
+        return this
     }
 }
