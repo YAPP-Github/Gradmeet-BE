205c07a1f30862b6cfe32c71a5a62d11f4a0cba4
Sujung Shin <113707388+chock-cho@users.noreply.github.com>
2025-07-13 21:25:58 +0900

    [YS-506] refactor: 자동완성 API - 동일 요청에 대한 캐싱 처리로 리팩토링 (#149)
    * feature: add cache gateway interface for autocomplete
    
    * feature: add implementation of cache gateway interface
    
    * feature: add cache config to usecase class layer
    
    * test: add test case for cache config verification
    
    * style: apply ktlintCheck to meet the ktlint-style
    
    * refactor: add Exception case for redis cache external error

--- domain/src/main/kotlin/com/dobby/gateway/CacheGateway.kt
+++ domain/src/main/kotlin/com/dobby/gateway/CacheGateway.kt
@@ -3,9 +3,11 @@ package com.dobby.gateway
 interface CacheGateway {
     fun <T> getObject(key: String, clazz: Class<T>): T?
     fun get(key: String): String?
+    fun getAutoComplete(key: String): List<String>?
     fun setObject(key: String, value: Any)
     fun set(key: String, value: String)
     fun setCode(key: String, value: String)
+    fun setAutoComplete(key: String, value: List<String>)
     fun incrementRequestCount(key: String)
     fun evict(key: String)
 }
