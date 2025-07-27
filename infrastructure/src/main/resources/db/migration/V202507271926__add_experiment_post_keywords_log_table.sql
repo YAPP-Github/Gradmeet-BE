1b0c2b5bb88ef1c07947067d56b59a17cc2265a3
jisu <dlawotn3@naver.com>
2025-07-27 19:29:43 +0900

    chore: add `experiment_post_keywords_log` table SQL file

---
+++ infrastructure/src/main/resources/db/migration/V202507271926__add_experiment_post_keywords_log_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE experiment_post_keywords_log (
+    experiment_post_keywords_log_id CHAR(13) NOT NULL PRIMARY KEY,
+    member_id CHAR(13) NOT NULL,
+    response TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    CONSTRAINT fk_experiment_post_keywords_log_member
+        FOREIGN KEY (member_id) REFERENCES member (member_id)
+) COLLATE = utf8mb4_unicode_ci;
