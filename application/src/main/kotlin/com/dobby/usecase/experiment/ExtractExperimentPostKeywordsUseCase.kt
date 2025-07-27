8f76558d68d7ff8356cca58494eeebd2df493c33
jisu <dlawotn3@naver.com>
2025-07-27 20:40:13 +0900

    feat: add daily usage limit validation for experiment post keywords

--- application/src/main/kotlin/com/dobby/usecase/experiment/ExtractExperimentPostKeywordsUseCase.kt
+++ application/src/main/kotlin/com/dobby/usecase/experiment/ExtractExperimentPostKeywordsUseCase.kt
@@ -1,5 +1,6 @@
 package com.dobby.usecase.experiment
 
+import com.dobby.exception.ExperimentPostKeywordsDailyLimitExceededException
 import com.dobby.gateway.experiment.ExperimentKeywordExtractionGateway
 import com.dobby.gateway.experiment.ExperimentPostKeywordsLogGateway
 import com.dobby.gateway.member.MemberGateway
@@ -7,6 +8,7 @@ import com.dobby.model.experiment.ExperimentPostKeywordsLog
 import com.dobby.model.experiment.keyword.ExperimentPostKeyword
 import com.dobby.usecase.UseCase
 import com.dobby.util.IdGenerator
+import com.dobby.util.TimeProvider
 
 class ExtractExperimentPostKeywordsUseCase(
     private val experimentKeywordExtractionGateway: ExperimentKeywordExtractionGateway,
@@ -14,6 +16,10 @@ class ExtractExperimentPostKeywordsUseCase(
     private val memberGateway: MemberGateway,
     private val idGenerator: IdGenerator
 ) : UseCase<ExtractExperimentPostKeywordsUseCase.Input, ExtractExperimentPostKeywordsUseCase.Output> {
+
+    companion object {
+        private const val DAILY_USAGE_LIMIT = 2
+    }
 
     data class Input(
         val memberId: String,
@@ -26,6 +32,8 @@ class ExtractExperimentPostKeywordsUseCase(
 
     override fun execute(input: Input): Output {
         val member = memberGateway.getById(input.memberId)
+        validateDailyUsageLimit(input.memberId)
+
         val experimentPostKeyword = experimentKeywordExtractionGateway.extractKeywords(input.text)
         val log = ExperimentPostKeywordsLog.newExperimentPostKeywordsLog(
             id = idGenerator.generateId(),
@@ -35,5 +43,21 @@ class ExtractExperimentPostKeywordsUseCase(
 
         experimentPostKeywordsGateway.save(log)
         return Output(experimentPostKeyword)
+    }
+
+    private fun validateDailyUsageLimit(memberId: String) {
+        val today = TimeProvider.currentDateTime().toLocalDate()
+        val startOfDay = today.atStartOfDay()
+        val endOfDay = today.plusDays(1).atStartOfDay()
+
+        val todayUsageCount = experimentPostKeywordsGateway.countByMemberIdAndCreatedAtBetween(
+            memberId = memberId,
+            start = startOfDay,
+            end = endOfDay
+        )
+
+        if (todayUsageCount >= DAILY_USAGE_LIMIT) {
+            throw ExperimentPostKeywordsDailyLimitExceededException
+        }
     }
 }
