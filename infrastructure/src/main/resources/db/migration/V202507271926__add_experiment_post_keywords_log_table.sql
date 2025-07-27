2877e91cecc7ed9ad6a8e663d8fe017f7e521ae0
jisu <dlawotn3@naver.com>
2025-07-27 21:04:57 +0900

    chore: add idx_experiment_keywords_log for covering index

--- infrastructure/src/main/resources/db/migration/V202507271926__add_experiment_post_keywords_log_table.sql
+++ infrastructure/src/main/resources/db/migration/V202507271926__add_experiment_post_keywords_log_table.sql
@@ -6,3 +6,5 @@ CREATE TABLE experiment_post_keywords_log (
     CONSTRAINT fk_experiment_post_keywords_log_member
         FOREIGN KEY (member_id) REFERENCES member (member_id)
 ) COLLATE = utf8mb4_unicode_ci;
+
+CREATE INDEX idx_experiment_keywords_log ON experiment_post_keywords_log (member_id, created_at);
