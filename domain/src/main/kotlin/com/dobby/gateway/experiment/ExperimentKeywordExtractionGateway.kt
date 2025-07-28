aed22a065a9237b660298a4a6b3602830f627a86
Jisu Lim <69844138+Ji-soo708@users.noreply.github.com>
2025-07-28 18:01:07 +0900

    revert: Revert "[YS-509] feat: 공고 키워드 자동완성 기능 구현 (#150)" (#159)
    This reverts commit 3456a5e510884d82e48a68e32d35ab17cee072ac.

--- domain/src/main/kotlin/com/dobby/gateway/experiment/ExperimentKeywordExtractionGateway.kt
+++
@@ -1,7 +0,0 @@
-package com.dobby.gateway.experiment
-
-import com.dobby.model.experiment.keyword.ExperimentPostKeyword
-
-interface ExperimentKeywordExtractionGateway {
-    fun extractKeywords(text: String): ExperimentPostKeyword
-}
