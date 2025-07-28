57a62a06e0865bbfc3ecaed7700ceaf32031cc01
jisu <dlawotn3@naver.com>
2025-07-28 14:32:12 +0900

    refactor: improve ContentLengthRequestInterceptor readability

--- infrastructure/src/main/kotlin/com/dobby/external/feign/ContentLengthRequestInterceptor.kt
+++ infrastructure/src/main/kotlin/com/dobby/external/feign/ContentLengthRequestInterceptor.kt
@@ -6,14 +6,16 @@ import org.springframework.stereotype.Component
 
 @Component
 class ContentLengthRequestInterceptor : RequestInterceptor {
+    companion object {
+        private const val CONTENT_LENGTH_HEADER = "Content-Length"
+        private const val GOOGLE_OAUTH_HOST = "oauth2.googleapis.com"
+    }
+
     override fun apply(requestTemplate: RequestTemplate) {
-        if (requestTemplate.feignTarget().url().contains("oauth2.googleapis.com")) {
+        val targetUrl = requestTemplate.feignTarget().url()
+        if (targetUrl.contains(GOOGLE_OAUTH_HOST) && requestTemplate.method() == "POST") {
             requestTemplate.body("")
             requestTemplate.header(CONTENT_LENGTH_HEADER, "0")
         }
-    }
-
-    companion object {
-        private const val CONTENT_LENGTH_HEADER = "Content-Length"
     }
 }
